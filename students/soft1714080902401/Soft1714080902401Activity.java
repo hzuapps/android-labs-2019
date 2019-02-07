2aea6f0f62f49bf4d6a4e3577d82564bf5503fef
kaluozi <784745401@qq.com>
2019-02-08 00:40:39 +0800

    #1#kaluozi 第一次实验

---
+++ students/soft1714080902401/Soft1714080902401Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.soft1714080902401;
+
+public class Soft1714080902401Activity {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
