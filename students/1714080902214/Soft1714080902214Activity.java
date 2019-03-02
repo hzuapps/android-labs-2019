4696f89abba3cd35d5fd6067ffa8ff9f1e6f6ec9
chicken-noodle <48144167+chicken-noodle@users.noreply.github.com>
2019-03-02 17:37:48 +0800

    Update Soft1714080902214Activity.java

--- students/1714080902214/Soft1714080902214Activity.java
+++ students/1714080902214/Soft1714080902214Activity.java
@@ -1,7 +1,7 @@
 package edu.hzuapps.androidlabs.soft1714080902214;
 
 public class Soft1714080902214Activity {
-    public static void main(String args){
+    public static void main(String args[]){
         System.out.println("Hello World!");
     }
 }
