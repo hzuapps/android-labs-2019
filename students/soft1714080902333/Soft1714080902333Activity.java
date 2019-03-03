ff42f36018aeb743647f0517fa0188dec1bee4ae
Seechai47 <alex.6952oy@gmail.com>
2019-03-03 11:09:26 +0800

    #1 #33 第一次实验

--- students/soft1714080902333/Soft1714080902333Activity.java
+++ students/soft1714080902333/Soft1714080902333Activity.java
@@ -1,6 +1,6 @@
 package edu.hzuapps.androidlabs.soft1714080902333;
 
-public class Soft1714080902333 {
+public class Soft1714080902333Activity {
 	public static void main(String[] args) {
 		System.out.println("!");
 	}
