1cdd7191d87f7b17d241c4a26f397fe86752f5c4
skyzzk <943816847@qq.com>
2019-03-03 10:13:30 +0800

    com1714080901240helloworld

---
+++ students/com1714080901240/com1714080901240helloworld.java
@@ -0,0 +1,11 @@
+package hello.com;
+
+public class hellojava {
+	public static void main(String[] args) {
+		String str="hello! world";
+		String newstr=str.replace("s","D");
+		System.out.println(str);
+		System.out.println(newstr);
+	}
+
+}
