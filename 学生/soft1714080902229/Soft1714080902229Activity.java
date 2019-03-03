33bb6ce8b309426c2cab7857ad7002cc1e8e3a26
hodor629 <48144149+hodor629@users.noreply.github.com>
2019-03-03 21:49:30 +0800

    Create Soft1714080902229Activity.java

---
+++ 学生/soft1714080902229/Soft1714080902229Activity.java
@@ -0,0 +1,10 @@
+package edu.hzuapps.androidlabs.soft1714080902229;
+
+public class Soft1714080902229Activity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
