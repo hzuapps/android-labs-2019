872263daf618b21c24041a5aee7ebb80d153170a
YohnA <846975652@qq.com>
2019-03-03 21:45:37 +0800

    创建第一个类

--- students/com1714080901235/Com1714080901235Activity.java
+++ students/com1714080901235/Com1714080901235Activity.java
@@ -1,5 +1,7 @@
+package edu.hzuapps.androidlabs.com1714080901235;
+
 public class Com1714080901235Activity {
-	public static void main(String []a) {
-		
+	public static void main(String []args) {
+		System.out.println("YohnA");
 	}
 }
