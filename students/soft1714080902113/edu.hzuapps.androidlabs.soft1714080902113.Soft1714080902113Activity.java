cd30b73396f99570f3b15f692a09f702461989bd
zhuanglinwu <342663636@qq.com>
2019-03-03 13:37:18 +0800

    创建第一个类

--- students/soft1714080902113/edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java
+++ students/soft1714080902113/edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java
@@ -1,7 +1,7 @@
-public class Edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java
-{
-         public static void main(String[] args)
-       {
-             System.out.printf("Hello World!");
-        }
-} 
+package edu.hzuapps.androidlabs.soft1714080902113;
+
+public class Soft1714080902113Activity {
+    public static void main(String args[]){
+        System.out.printf("Hello World!");
+    }
+}
