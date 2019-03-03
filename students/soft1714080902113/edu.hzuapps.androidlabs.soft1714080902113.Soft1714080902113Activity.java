4b77a762589a05040a72a43b4c0f87a17377e2c6
zhuanglinwu <342663636@qq.com>
2019-03-03 12:56:09 +0800

    第一次实验

--- students/soft1714080902113/edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java
+++ students/soft1714080902113/edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java
@@ -1,5 +1,7 @@
-public class Edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java{
-    public static void main(String[] args){
-        System.out.printf("Hello World!");
-    }
-}
+public class Edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java
+{
+         public static void main(String[] args)
+       {
+             System.out.printf("Hello World!");
+        }
+} 
