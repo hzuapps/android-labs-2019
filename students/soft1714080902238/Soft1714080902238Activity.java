ffc36163186ba558f392ec74c5da4647b6201fc7
PinkDevil <48143945+756612440@users.noreply.github.com>
2019-03-03 20:49:59 +0800

    Create Soft1714080902238Activity.java

---
+++ students/soft1714080902238/Soft1714080902238Activity.java
@@ -0,0 +1,6 @@
+package edu.hzuapps.androidlabs.soft1714080902238
+public class Soft1714080902238Activity{
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}
