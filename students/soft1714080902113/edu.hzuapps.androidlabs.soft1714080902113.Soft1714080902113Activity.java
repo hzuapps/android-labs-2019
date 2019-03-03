67e2a870624496e988ddc31631cd604b61ac4589
342663636 <46732785+342663636@users.noreply.github.com>
2019-03-03 12:10:14 +0800

    Update edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java

--- students/soft1714080902113/edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java
+++ students/soft1714080902113/edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java
@@ -1,4 +1,4 @@
-public class HelloWorld{
+public class Edu.hzuapps.androidlabs.soft1714080902113.Soft1714080902113Activity.java{
     public static void main(String[] args){
         System.out.printf("Hello World!");
     }
