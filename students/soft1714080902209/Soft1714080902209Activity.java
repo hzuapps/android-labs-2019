457c824685d21da48ffdd9ad7ce71dce0c9743dd
Lam <lam@Lams-MB.local>
2019-03-04 08:36:21 +0800

    实验1——创建第一个类

---
+++ students/soft1714080902209/Soft1714080902209Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.soft1714080902209;
+
+public class Soft1714080902209Activity {
+    public static void main(String args[]){
+        System.out.println("hello, world");
+    }
+}
