4a564d164ca7411f5f346967a595311b085c4226
cqr22 <1309382951@qq.com>
2019-03-06 21:47:30 +0800

    创建第一个类

---
+++ students/soft1706081301317/Soft1706081301317Activity.java
@@ -0,0 +1,10 @@
+package edu.hzuapps.androidlabs.soft1706081301317;
+
+public class Soft1706081301317Activity {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.print("helloworld!");
+	}
+
+}
