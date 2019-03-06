f74214dd3b27dd43f1618e89f3f2a12fba77a2d0
yingbabywhen <1282418412@qq.com>
2019-03-06 22:15:51 +0800

    创建一个类

--- students/soft1714080902309/Soft1714080902309Activity.java
+++ students/soft1714080902309/Soft1714080902309Activity.java
@@ -1,6 +1,6 @@
 package edu.hzuapps.androidlabs.soft1714080902309;
 
-public class Main {
+public class Soft1714080902309Activity {
 
     public static void main(String[] args) {
         System.out.println("Hello Word!");
