b7412fc1fa42fddc7e825f1838c67de9715a457b
TWRT <983793969@qq.com>
2019-03-07 12:35:41 +0800

    创建第一个类

---
+++ students/soft1714080902311/soft1714080902311Activity.java
@@ -0,0 +1,11 @@
+package edu.hzuapps.androidlabs.soft1714080902311;
+
+public class soft1714080902311Activity{
+
+public static void main(String args[]){
+
+System.out.println("Touch the sky");
+
+}
+
+}
