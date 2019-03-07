9d4f3d5bc452382585eaa47f7b75a6c9a9dae157
ridworld <48148324+ridworld@users.noreply.github.com>
2019-03-07 08:25:53 +0800

    Add files via upload

---
+++ students/Soft1714080902239/Soft1714080902239.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.android.soft1714080902239;
+
+public class Soft1714080902239 {
+	String name;
+	int age;
+	float weight;
+	public static void main(String[] args){
+		Soft1714080902239 A=new Soft1714080902239();
+		A.name="myself";
+		A.age=20;
+		A.weight=45.6f;
+	}
+}
