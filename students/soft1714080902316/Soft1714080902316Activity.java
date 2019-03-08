8e02d86c48e5ef7191320d41a5ba1cf0bfa125e8
DengXF1225 <374526876@qq.com>
2019-03-08 23:57:05 +0800

    创建第一个类

---
+++ students/soft1714080902316/Soft1714080902316Activity.java
@@ -0,0 +1,8 @@
+package edu.hzuapps.androidlabs.soft1714080902316;
+
+public class Soft1714080902316Activity {
+    public static void main(String[] args)
+    {
+        System.out.print("Hello World !");
+    }
+}
