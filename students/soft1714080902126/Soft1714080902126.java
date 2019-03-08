1b2c38f4da234b315705988356336eb402f64df8
aflyingwood <panqiang129354416@qq.com>
2019-03-08 19:19:52 +0800

    #1 第1次实验

--- students/soft1714080902126/Soft1714080902126.java
+++ students/soft1714080902126/Soft1714080902126.java
@@ -1,5 +1,5 @@
 public class Com1714080902126Activity {
        public static void main(String[] args) {
-                 System.out.println("Hello,World!!!");
+                 System.out.println("Hello,Android World!");
        }
 }
