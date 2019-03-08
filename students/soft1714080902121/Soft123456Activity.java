b39756ab8cd0f9b77574d096b080ce51cd091ae9
VOEZ21 <48143643+VOEZ21@users.noreply.github.com>
2019-03-08 22:09:29 +0800

    Update Soft123456Activity.java

--- students/soft1714080902121/Soft123456Activity.java
+++ students/soft1714080902121/Soft123456Activity.java
@@ -1,4 +1,7 @@
-package com.example.k00.myapplication.edu.hzuapps.androidlabs.soft1714080902121;
+package edu.hzuapps.androidlabs.soft1714080902121;
 
-public class Soft1714080902121Activity {
+public class Soft1714080902121 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
