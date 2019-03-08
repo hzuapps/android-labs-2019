79823b303228d8806f8aa5ac999acc48f1ee9950
Trial0720 <506941004@qq,com>
2019-03-08 14:56:30 +0800

    #1 Trial0720 第一次实验

---
+++ students/soft1714080902423/soft1714080902423Activity.java
@@ -0,0 +1,9 @@
+package Number£»
+public class Frist{
+	static String s1="hello";
+	public static void main (String[] args){
+		String s2="JAVA";
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
