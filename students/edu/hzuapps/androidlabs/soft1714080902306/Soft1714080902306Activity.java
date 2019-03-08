2ec4a908ecaf77d60ef7bdca4ec541888e8c066e
1714080902306 <1240780563@qq.com>
2019-03-08 19:45:02 +0800

    #1 #1714080902306 第一次实验

---
+++ students/edu/hzuapps/androidlabs/soft1714080902306/Soft1714080902306Activity.java
@@ -0,0 +1,9 @@
+package edu.hzuapps.androidlabs.soft1714080902306;
+
+public class Soft1714080902306Activity {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+
+}
