1286847f74bde040d2ca5b850bece8e6da96b56d
csl275 <1564399427@qq.com>
2019-03-08 22:52:20 +0800

    # 1 #第一次实验
    # 1 #第一次实验

---
+++ students/com1714080901139/edu.hzuapps.androidlabs.com1714080901139Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.Com1714080901139Activity;
+
+public class edu.hzuapps.androidlabs.Com1714080901139Activity{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
