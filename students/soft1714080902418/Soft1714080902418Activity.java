d8bc3e7bba60649c1708e8ebc5fe628338c8a420
linsimin <1491885028@qq.com>
2019-03-08 20:03:45 +0800

    #1 #linsimin 第一次实验

---
+++ students/soft1714080902418/Soft1714080902418Activity.java
@@ -0,0 +1,8 @@
+package edu.hzuapps.androidlabs.soft1714080902418;
+
+public class Soft1714080902418Activity {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,JAVA!");
+    }
+}
