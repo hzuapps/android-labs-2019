05e17e43f91b3a6a4893b73745f340fcb6c6ffa8
GinSunx <mzginsun@qq.com>
2019-03-08 22:22:23 +0800

    #1 #02 第一次实验

---
+++ students/soft1714080902302/Soft1714080902302Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.Soft1714080902302;
+
+public class Soft1714080902302Activity {
+    public sattic void main(String[] args){
+        System.out.println("hello world!")
+    }
+}
