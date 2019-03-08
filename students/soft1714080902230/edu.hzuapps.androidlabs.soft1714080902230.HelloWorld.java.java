26c8f7139238744538a11350bd710c916a104d28
alicekagiyama <48143965+alicekagiyama@users.noreply.github.com>
2019-03-08 20:47:08 +0800

    Add files via upload

---
+++ students/soft1714080902230/edu.hzuapps.androidlabs.soft1714080902230.HelloWorld.java.java
@@ -0,0 +1,8 @@
+package package edu.hzuapps.androidlabs.soft1714080902230;
+
+public class Soft1714080902230Activity {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
