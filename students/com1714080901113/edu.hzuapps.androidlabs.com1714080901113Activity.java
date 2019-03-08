e13a7ccf5081f902ac4b33e408fee3c9b616786c
as912256 <asd123>
2019-03-08 23:20:19 +0800

    #1#第一次实验

---
+++ students/com1714080901113/edu.hzuapps.androidlabs.com1714080901113Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.Com1714080901113Activity;
+
+public class edu.hzuapps.androidlabs.Com1714080901113Activity{
+    public static void main(String[] args){
+        System.out.println("Hello      world!!!");
+    }
+}
