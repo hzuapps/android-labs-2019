c37d7565b87bac038b54799d97b87c89901ee0f8
zengjundong <794102804@qq.com>
2019-03-09 16:56:39 +0800

    创建第一个类

---
+++ students/com1714080901131/Com1714080901131Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.com1714080901131;
+
+public class Com1714080901131 {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
