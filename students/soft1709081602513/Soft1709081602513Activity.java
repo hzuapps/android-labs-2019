885c7636b990471eeca43e110469be0815eeba08
your name <your email>
2019-03-09 20:20:46 +0800

    创建第一个类

--- students/soft1709081602513/Soft1709081602513Activity.java
+++ students/soft1709081602513/Soft1709081602513Activity.java
@@ -1,20 +1,4 @@
-package cn.hzu;
+package edu.hzuapps.androidlabs.soft1709081602513;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@EnableAutoConfiguration
 public class Soft1709081602513Activity {
-
-    @RequestMapping("/hello")
-    String home() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(Soft1709081602513Activity.class, args);
-    }
-
 }
