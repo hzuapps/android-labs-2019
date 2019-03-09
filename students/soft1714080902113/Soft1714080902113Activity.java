eaff2dd509707bc024b547f0dcef7981f473c95f
zhuanglinwu <342663636@qq.com>
2019-03-09 12:38:27 +0800

    #1 第一次实验

---
+++ students/soft1714080902113/Soft1714080902113Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.soft1714080902113;
+
+public class Soft1714080902113Activity {
+    public static void main(String args[]){
+        System.out.printf("Hello World!");
+    }
+}
