19a617d2d59228a8742cae70f1d2739c4324e272
cqr22 <1309382951@qq.com>
2019-03-09 09:43:20 +0800

    实验1第一个类

--- students/soft1706081301317/Soft1706081301317Activity.java
+++ students/soft1706081301317/Soft1706081301317Activity.java
@@ -3,7 +3,6 @@ package edu.hzuapps.androidlabs.soft1706081301317;
 public class Soft1706081301317Activity {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
 		System.out.print("helloworld!");
 	}
 
