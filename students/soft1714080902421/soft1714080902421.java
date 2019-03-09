f4a410add3bfd0918b49fae1aa2cf905f518a4cd
Eheech <406391862@qq.com>
2019-03-09 16:31:48 +0800

    #1 Rhh第一次实验

--- students/soft1714080902421/soft1714080902421.java
+++ students/soft1714080902421/soft1714080902421.java
@@ -1,3 +1,17 @@
-pulic class soft1714080902421{
+public class Soft1714080902421Activity{
 
+
+ 
+	public static void main(String[] args) {
+
+
+ 
+		System.out.println("hello world");
+
+
+ 
+	}
+
+
+ 
 }
