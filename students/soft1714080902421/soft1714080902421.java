463548bba41c715fedcc7ef7e526a87e25c563a4
Eheech <406391862@qq.com>
2019-03-09 16:50:06 +0800

    #1 #159 第一次实验

--- students/soft1714080902421/soft1714080902421.java
+++ students/soft1714080902421/soft1714080902421.java
@@ -1,13 +1,11 @@
 public class Soft1714080902421Activity{
 
-
  
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+              {
 
  
 		System.out.println("hello world");
-
 
  
 	}
