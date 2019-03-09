7b51e834321822bcb5ffe0a7c5a2a51c9eba714b
Swag7zai <48141774+Swag7zai@users.noreply.github.com>
2019-03-09 09:15:51 +0800

    Update and rename Com1713071002108.java to Com1713071002108activity.java

--- students/com1713071002108/Com1713071002108.java
+++
@@ -1,4 +0,0 @@
-public class Com1713071002108{
-public static void main(String[] args){
-System.out.printin("hello world");}
-}
---
+++ students/com1713071002108/Com1713071002108activity.java
@@ -0,0 +1,6 @@
+package edu.hzuapps.androidlabs.com1713071002108;
+
+public class Com1713071002108Activity{
+public static void main(String[] args){
+System.out.printin("hello world");}
+}
