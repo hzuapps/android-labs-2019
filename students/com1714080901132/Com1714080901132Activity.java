e9b4932a4d31a3e28a42818662184cf78999da7c
xaioZe <48142604+xaioZe@users.noreply.github.com>
2019-03-09 10:16:29 +0800

    Update Com1714080901132Activity.java

--- students/com1714080901132/Com1714080901132Activity.java
+++ students/com1714080901132/Com1714080901132Activity.java
@@ -1,8 +1,6 @@
 public class Com1714080901132Activity{
-
-   public static void main(String[]args){
-           System.out.println("Hello World!");
-   }  
-   
-
-}
+	     public static void main(String[]args£©{
+	       System.out.println("Hello World");
+	   }
+	
+	}
