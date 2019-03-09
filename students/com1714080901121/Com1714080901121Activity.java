48a3133279aad7030e5fb5210d4fc357a6b2fcfa
odysseyh <5253833391@qq.com>
2019-03-09 09:50:10 +0800

    创建第一个类

---
+++ students/com1714080901121/Com1714080901121Activity.java
@@ -0,0 +1,9 @@
+package edu.hzuapps.androidlabs.com1714080901121;
+
+public class Com1714080901121Activity {
+
+        public static void main(String[] args) {
+            System.out.println("hello world!");
+        }
+    }
+
