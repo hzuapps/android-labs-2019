7b28a811d7e2381b4ff764a1bfbc292df3fd31de
SunJiashun <316218906@qq.com>
2019-03-09 09:42:30 +0800

    #1 #SunJiashun 重新提交实验1

--- students/soft1509081602625/soft1509081602625Activity.java
+++ students/soft1509081602625/soft1509081602625Activity.java
@@ -1,3 +1,7 @@
 package edu.hzuapps.androidlabs.soft1509081602625
+
 public class soft1509081602625Activity {
-};
+
+public static void main(String[] args) {
+			
+			System.out.println("hello world!");};
