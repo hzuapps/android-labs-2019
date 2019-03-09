2ebdffa573a78457117b55a51623b5ca59af82e0
XIAODINGD <850243182@qq.com>
2019-03-09 12:08:11 +0800

    Soft1714080902331Activity

--- students/soft1714080902331/Soft1714080902331Activity.java
+++ students/soft1714080902331/Soft1714080902331Activity.java
@@ -2,6 +2,7 @@ package edu.hzuapps.myapplication;
 
 public class Soft1714080902331Activity {
     public static void main() {
-        System.out.print("Hello World!");}
+        System.out.println("Hello World!");
+    }
 
 }
