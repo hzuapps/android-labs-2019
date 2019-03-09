e7b209d940fffd551b270a1739f2bc01d8efe334
Game111fff <964851688@qq.com>
2019-03-09 10:49:28 +0800

    #1 Game111fff 第一次实验

--- students/soft1714080902312/Soft1714080902312Activity.java
+++ students/soft1714080902312/Soft1714080902312Activity.java
@@ -1,1 +1,7 @@
-edu.hzuapps.androidlabs.soft123456.
+package edu.hzuapps.androidlabs.soft1714080902312;
+
+public class Soft1714080902312Activity {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}
