5305dcb9f566cc34b8d01a145403f411be3d5035
Ci-ing <1666811664@qq.com>
2019-03-12 22:40:08 +0800

    #1 #实验一

---
+++ students/soft1714080902339/Soft1714080902339Activity.java
@@ -0,0 +1,9 @@
+package edu.hzuapps.androidlabs.soft1714080902339;
+
+public class Soft1714080902339Activity  {
+	public static void main( String[] args )
+	{
+		//¥Ú”°—ß∫≈
+		System.out.println( "I'm Soft1714080902339.\n" );
+	}
+}
