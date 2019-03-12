14e45d3997eb27c1376397c1d3b2acf9243a30a5
IMMamba <3464332862@qq.com>
2019-03-12 19:22:18 +0800

    实验二

--- students/Com1714080901110Activity/Com1714080901110Activity.java
+++
@@ -1,5 +0,0 @@
-public class Com1714080901110Activity{
-    public static void main(String[]args){
-        System.out.println("Hello World");
-   }
-}
---
+++ students/edu/hzuapps/androidlabs/com1714080901110/Com1714080901110.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.edu.hzuapps.androidlabs.com1714080901110;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class Com1714080901110 extends Activity {
+
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mylayout);
+    }
+}
