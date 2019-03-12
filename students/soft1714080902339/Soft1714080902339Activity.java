9f7a8d58374976bf0011ba315375bc2032299395
Ci-ing <48150150+Ci-ing@users.noreply.github.com>
2019-03-12 22:44:43 +0800

    创建第一个类,并打印学号

--- students/soft1714080902339/Soft1714080902339Activity.java
+++ students/soft1714080902339/Soft1714080902339Activity.java
@@ -3,7 +3,7 @@ package edu.hzuapps.androidlabs.soft1714080902339;
 public class Soft1714080902339Activity  {
 	public static void main( String[] args )
 	{
-		//创建第一个类,打印学号
+		//创建第一个类,并打印学号 
 		System.out.println( "I'm Soft1714080902339.\n" );
 	}
 }
