b1b099aed26cc9c5ff53c975058fb6047ab5ba31
SunJiashun <48149605+SunJiashun@users.noreply.github.com>
2019-03-12 21:14:25 +0800

    Delete soft1509081602625Activity.java

--- students/soft1509081602625/soft1509081602625Activity.java
+++
@@ -1,7 +0,0 @@
-package edu.hzuapps.androidlabs.soft1509081602625
-
-public class soft1509081602625Activity {
-
-public static void main(String[] args) {
-			
-			System.out.println("hello world!");};
