15d978e78f73173dce9e215dc21eb1d43545c5ad
Ci-ing <48150150+Ci-ing@users.noreply.github.com>
2019-03-12 22:43:37 +0800

    修改乱码了的代码

--- students/soft1714080902339/Soft1714080902339Activity.java
+++ students/soft1714080902339/Soft1714080902339Activity.java
@@ -3,7 +3,7 @@ package edu.hzuapps.androidlabs.soft1714080902339;
 public class Soft1714080902339Activity  {
 	public static void main( String[] args )
 	{
-		//��ӡѧ��
+		//创建第一个类,打印学号
 		System.out.println( "I'm Soft1714080902339.\n" );
 	}
 }
