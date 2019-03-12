60d62df6cecf51abd01ff1c91cd1a5f3e73e84a3
HanXuanMo <a447899372@163.com>
2019-03-12 21:07:06 +0800

    提交实验二

---
+++ students/soft1714080902110/java/edu/hzuapps/androidlabs/soft1714080902110/RankList.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.soft1714080902110;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RankList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rank_list);
+    }
+}
