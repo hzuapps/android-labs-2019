dab5073a923541ab4c32a03881591cb7ef7e99e6
luxiaoshanhhh <2682786040@qq.com>
2019-03-13 16:57:28 +0800

    提交实验二代码

---
+++ students/soft1709081602603/app/src/main/java/edu/hzuapps/androidlabs/soft1709081602603/Soft1709081602603_Activity.java
@@ -0,0 +1,17 @@
+package edu.hzuapps.androidlabs.soft1709081602603;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import com.example.soft1709081602603.R;
+
+public class Soft1709081602603_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1709081602603_);
+    }
+}
