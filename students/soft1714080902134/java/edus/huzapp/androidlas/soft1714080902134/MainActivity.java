96ad6d99904dcfdf723ee77310dff45b25f67a2e
17727263252 <869991048@qq.com>
2019-03-13 22:13:56 +0800

    第二次安卓实验

---
+++ students/soft1714080902134/java/edus/huzapp/androidlas/soft1714080902134/MainActivity.java
@@ -0,0 +1,13 @@
+package edus.huzapp.androidlas.soft1714080902134;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
