694a9820079adceacdb7dc59e616860879d94701
HuangGuiguang <1614981899@qq.com>
2019-03-13 09:40:08 +0800

    #2 提交实验二代码

--- students/soft1714080902131/soft1714080902131Activity.java
+++ students/soft1714080902131/soft1714080902131Activity.java
@@ -1,6 +1,5 @@
-
-public class soft1714080902131Activity{
+public class Soft1714080902131Activity{
 	public static void main(String[] args){
-		System.out.println("hello£¬world£¡");
+		System.out.println("Hello£¬world£¡");
 	}
 }
