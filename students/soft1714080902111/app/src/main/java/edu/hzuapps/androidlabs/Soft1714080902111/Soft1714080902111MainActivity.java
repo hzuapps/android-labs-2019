5278a9130a5716d9800a2759561dc0cf583c6d6f
Wuhaochao <582830851@qq.com>
2019-03-15 01:01:32 +0800

    实验二

---
+++ students/soft1714080902111/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902111/Soft1714080902111MainActivity.java
@@ -0,0 +1,27 @@
+package edu.hzuapps.androidlabs.Soft1714080902111;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import edu.hzuapps.androidlabs.Soft1714080902111.R;
+
+public class Soft1714080902111MainActivity extends AppCompatActivity {
+
+    private Button mBtnTextView;  // define a text view button
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft1714080902111mainactivity);
+        mBtnTextView = findViewById(R.id.btnTextView1);
+        mBtnTextView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(Soft1714080902111MainActivity.this, Soft1714080902111TestActivity.class);
+                startActivity(intent);
+            }
+        });
+    };
+
+}
