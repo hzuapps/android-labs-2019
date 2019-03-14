3eda31ab1b1ce34e2ea442e7327365d972edeb65
Coke1e <z13542774735@163.com>
2019-03-14 22:47:50 +0800

    #2 #coke1e 第2次实验

---
+++ students/Soft1714080902403/app/src/main/java/com/example/myapplication/Soft1714080902403.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1714080902403 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902403);
+    }
+}
