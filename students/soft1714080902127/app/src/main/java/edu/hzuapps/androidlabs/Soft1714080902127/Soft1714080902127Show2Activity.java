9bf22e38b58a5f8211faeee344d79c09227616c5
410898194 <410898194@qq.com>
2019-03-14 22:43:42 +0800

    提交实验二代码

---
+++ students/soft1714080902127/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902127/Soft1714080902127Show2Activity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.Soft1714080902127;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1714080902127Show2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902127_show2activity);
+    }
+}
