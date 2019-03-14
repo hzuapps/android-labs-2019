06cbefc64496e1a6c3b97aefb37d0be137026150
DengXF1225 <374526876@qq.com>
2019-03-14 21:26:20 +0800

    第二次实验

---
+++ students/soft1714080902316/app/src/main/java/com/example/edu/hzuapps/androidlabs/soft1714080902316/Soft1714080902316.java
@@ -0,0 +1,15 @@
+package com.example.edu.hzuapps.androidlabs.soft1714080902316;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.edu.hzuapps.androidlabs.R;
+
+public class Soft1714080902316 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902316);
+    }
+}
