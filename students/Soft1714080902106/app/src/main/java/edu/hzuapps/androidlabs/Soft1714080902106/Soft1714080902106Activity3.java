c25a475026dd3b9eb2f29fea2d4da28d7366f59d
1714080902106 <1921673732@qq.com>
2019-03-14 23:29:22 +0800

    提交实验2代码

---
+++ students/Soft1714080902106/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902106/Soft1714080902106Activity3.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.Soft1714080902106;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1714080902106Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902106_activity3);
+    }
+}
