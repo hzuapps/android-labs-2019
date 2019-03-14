e4b6d42222b0cefc043df17c35594ed9eb8a7fb4
DengXF1225 <374526876@qq.com>
2019-03-14 21:42:50 +0800

    Delete Soft1714080902316Activity.java

--- students/soft1714080902316/Soft1714080902316Activity.java
+++
@@ -1,8 +0,0 @@
-package edu.hzuapps.androidlabs.soft1714080902316;
-
-public class Soft1714080902316Activity {
-    public static void main(String[] args)
-    {
-        System.out.print("Hello World !");
-    }
-}
