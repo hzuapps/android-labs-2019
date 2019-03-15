76ca92b7446b8329f4f33ba64e5aca63cf40f046
YHTION <2380126648@qq.com>
2019-03-15 10:48:13 +0800

    第二个实验

---
+++ students/soft1714080902104/java/com/hzuapps/androidlabs/soft1714080902104/Soft1714080902104Activity.java
@@ -0,0 +1,15 @@
+package com.hzuapps.androidlabs.soft1714080902104;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.menu.R;
+
+public class Soft1714080902104Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902104_activity);
+    }
+}
