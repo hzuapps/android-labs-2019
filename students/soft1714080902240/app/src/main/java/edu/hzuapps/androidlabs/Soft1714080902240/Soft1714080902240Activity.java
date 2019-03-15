4924a6c0a875bf8ae5cb5b67d5af0b42240bb99e
YZT6 <1765422809@qq.com>
2019-03-15 22:13:20 +0800

    #二 #YZT6 第二次实验

---
+++ students/soft1714080902240/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902240/Soft1714080902240Activity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.Soft1714080902240;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1714080902240Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902240_activity_main);
+    }
+}
