6533448014148047952a285ffc5fb6acfec4b55e
GinSunx <48149948+GinSunx@users.noreply.github.com>
2019-03-15 14:07:22 +0000

    Delete Soft1714080902302Activity.java

--- students/soft1714080902302/Soft1714080902302Activity.java
+++
@@ -1,7 +0,0 @@
-package edu.hzuapps.androidlabs.Soft1714080902302;
-
-public class Soft1714080902302Activity {
-    public sattic void main(String[] args){
-        System.out.println("hello world!")
-    }
-}
