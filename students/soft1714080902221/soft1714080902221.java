8f4dd5fef58448a4545dc0ec8164bb1df6331658
zhengrunkai <1256311705@qq.com>
2019-03-15 19:56:31 +0800

    Revert "实验2跳转页面"
    This reverts commit 400fe9fcb58aae71d31c28a839bbaab5f8a12283.

---
+++ students/soft1714080902221/soft1714080902221.java
@@ -0,0 +1,14 @@
+package students/soft1714080902221/Soft1714080902221Activity.java
+public class Student { 
+ 
+    private String name; 
+ 
+    private int age; 
+ 
+    public String getName() { 
+        return this.name; 
+    } 
+ 
+    public void setName(String name) { 
+        this.name = name; 
+    } 
