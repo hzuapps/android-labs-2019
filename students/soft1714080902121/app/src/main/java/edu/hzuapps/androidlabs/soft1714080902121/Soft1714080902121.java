2a3f8e16ef400603ac3952610da389a93fd1b077
VOEZ21 <893302438@qq.com>
2019-03-15 18:39:21 +0800

    #3

--- students/soft1714080902121/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902121/Soft1714080902121.java
+++
@@ -1,7 +0,0 @@
-package edu.hzuapps.androidlabs.soft1714080902121;
-
-public class Soft1714080902121 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
