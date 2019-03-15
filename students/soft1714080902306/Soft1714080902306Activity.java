b57fcd70cf551192075b800624d886349804972a
1714080902306 <48149975+1714080902306@users.noreply.github.com>
2019-03-15 23:19:07 +0800

    Delete Soft1714080902306Activity.java

--- students/soft1714080902306/Soft1714080902306Activity.java
+++
@@ -1,8 +0,0 @@
-package edu.hzuapps.androidlabs.soft1714080902306;
-
-public class Soft1714080902306Activity {
-	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
-	}
-
-}
