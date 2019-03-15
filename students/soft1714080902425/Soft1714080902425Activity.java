4430254634d6249d3a81491857b71de99861d969
AngleBeatQAQ <499352824@qq.com>
2019-03-15 14:42:59 +0800

    #1 #AngleBeatQAQ 第一次实验

--- students/soft1714080902425/Soft1714080902425Activity.java
+++ students/soft1714080902425/Soft1714080902425Activity.java
@@ -1,3 +1,6 @@
-package edu.hzuapps.androidlabs.soft1714080902425;
-
-public class Soft1714080902425Activity{}
+package com.edu.hzuapps.androidlabs.soft1714080902425
+public class Soft1714080902425Activity{
+	public static void main(String[] args) {
+		System.out.println("Hello Android");
+	}
+}
