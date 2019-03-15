f702f0c41c22e761103594651ada463b6e8eef09
hodor629 <48144149+hodor629@users.noreply.github.com>
2019-03-16 00:24:07 +0800

    Update Soft1714080902229Activity.java

--- students/Soft1714080902229/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902229/Soft1714080902229Activity.java
+++ students/Soft1714080902229/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902229/Soft1714080902229Activity.java
@@ -1,4 +1,4 @@
-package soft1714080902229.androidlabs.hzuapps.edu.myapplication.Soft1714080902229Activity;
+package edu.hzuapps.androidlabs.Soft1714080902229;
 
 /**
  * Created by k00 on 2019/3/14.
