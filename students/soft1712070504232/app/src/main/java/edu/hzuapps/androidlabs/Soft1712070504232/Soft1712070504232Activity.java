d0938e3661ab86e3280d25b51725025a28d1950d
Hzu-sick <1312203573@qq.com>
2019-03-15 21:10:55 +0800

    delete

---
+++ students/soft1712070504232/app/src/main/java/edu/hzuapps/androidlabs/Soft1712070504232/Soft1712070504232Activity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.Soft1712070504232;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Soft1712070504232Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1712070504232_activity);
+    }
+}
