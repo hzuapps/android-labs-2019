1517374bc743e1a5d1e96930ac24914a17473e3b
anxiangleisuhen <41566081+anxiangleisuhen@users.noreply.github.com>
2019-03-15 19:34:29 +0800

    第二个文件

---
+++ students/soft1714080902225/实验二/MyApplication5/app/src/main/java/com/edu/hzuapps/androidlabs/soft1714080902225activity/Soft1714080902225Activity.java
@@ -0,0 +1,13 @@
+package com.edu.hzuapps.androidlabs.soft1714080902225activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1714080902225Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902225_activity);
+    }
+}
