2c8b4c8e1d1d27552caef27eb0db1a5ee9934083
Lyeye <1719580234@qq.com>
2019-03-15 14:39:01 +0800

    #第二次实验

---
+++ students/soft1714080902408/Soft1714080902408.java
@@ -0,0 +1,7 @@
+package com.hzuandroid.soft1714080902408;
+
+public class Soft1714080902408 {
+    public static void main(String args[]){
+        System.out.println("My first Android class!");
+    }
+}
