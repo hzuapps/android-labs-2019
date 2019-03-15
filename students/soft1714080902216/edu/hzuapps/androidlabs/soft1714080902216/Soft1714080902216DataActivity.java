b6cc686cac2711044c62e69285e7088f2c2cbb90
KasenBob <826107692@qq.com>
2019-03-15 15:36:35 +0800

    KasenBob 第三次实验

---
+++ students/soft1714080902216/edu/hzuapps/androidlabs/soft1714080902216/Soft1714080902216DataActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.soft1714080902216;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1714080902216DataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902216_data);
+    }
+}
