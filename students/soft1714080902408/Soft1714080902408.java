a0eae04aa83210f2b0a888f1fe2e27a80df051bb
Lyeye <1719580234@qq.com>
2019-03-15 19:34:05 +0800

    #2 #第二次实验

---
+++ students/soft1714080902408/Soft1714080902408.java
@@ -0,0 +1,12 @@
+package com.hzuandroid.soft1714080902408;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Soft1714080902408 extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
