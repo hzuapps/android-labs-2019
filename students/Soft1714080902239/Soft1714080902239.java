fb9492b7226c1ce8e0e1c9ff1a5bf8635fab5a9f
ridworld <48148324+ridworld@users.noreply.github.com>
2019-03-15 11:44:05 +0800

    Add files via upload

--- students/Soft1714080902239/Soft1714080902239.java
+++ students/Soft1714080902239/Soft1714080902239.java
@@ -1,13 +1,25 @@
-package edu.hzuapps.android.soft1714080902239;
+package edu.hzuapps.androidapps;
 
-public class Soft1714080902239 {
-	String name;
-	int age;
-	float weight;
-	public static void main(String[] args){
-		Soft1714080902239 A=new Soft1714080902239();
-		A.name="myself";
-		A.age=20;
-		A.weight=45.6f;
-	}
-}
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Soft1714080902239 extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "edu.hzuapps.androidapps.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902239_activty);
+    }
+        public void sendMessage (View view){
+            Intent intent = new Intent(this, Soft1714080902239Activty.class);
+            EditText editText = (EditText) findViewById(R.id.editText);
+            String message = editText.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        }
+    }
