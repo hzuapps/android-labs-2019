7a7714ad0a84e495b87d019e85a0e4a17ff934f5
liutao2019 <1980661451@qq.com>
2019-03-15 21:05:38 +0800

    #第二次实验

---
+++ students/soft1714080902410/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.soft1714080902410;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
