96a049dc7a1d4ead742e92e7d8106ae525f44f84
hcp12345 <3132857267@qq.com>
2019-03-15 15:35:31 +0800

    #2 #hcp12345 第二次实验

--- students/soft1714080902431/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902431/soft1714080902431Activity.java
+++ students/soft1714080902431/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902431/soft1714080902431Activity.java
@@ -3,7 +3,7 @@ package edu.hzuapps.androidlabs.Soft1714080902431;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class soft1714080902431Activity extends Activity {
+public class Soft1714080902431Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
