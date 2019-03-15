c9942ec1b5c5ea39c68c3d668a4864fcacd346e8
wshrhhh <1412364676@qq.com>
2019-03-15 20:27:50 +0800

    #2 #325 第二次实验

--- students/soft1714080902227/Soft1714080902227Activity.java
+++ students/soft1714080902227/Soft1714080902227Activity.java
@@ -1,6 +1,6 @@
 package com.example.myapplication;
 
-public class Soft1714080902227Activity {
+public class soft1714080902227Activity {
     public static void main(String []args){
         System.out.println("666");
     }
