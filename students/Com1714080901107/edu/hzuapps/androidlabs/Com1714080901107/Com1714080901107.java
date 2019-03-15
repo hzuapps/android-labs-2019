39f2018d5847ab4a0df50b2bccf31865d7d8e555
huizi17shou <759765634@qq.com>
2019-03-15 18:24:35 +0800

    实验二

---
+++ students/Com1714080901107/edu/hzuapps/androidlabs/Com1714080901107/Com1714080901107.java
@@ -0,0 +1,31 @@
+package edu.hzuapps.androidlabs.Com1714080901107;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.myapplication.R;
+
+public class Com1714080901107 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
