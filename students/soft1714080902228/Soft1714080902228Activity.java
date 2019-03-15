68a3f81a150143ca63310238d96aa6c4438132de
Heywar <1197650307@qq.com>
2019-03-15 18:52:12 +0800

    第二次实验

--- students/soft1714080902228/Soft1714080902228Activity.java
+++ students/soft1714080902228/Soft1714080902228Activity.java
@@ -1,4 +1,13 @@
-package edu.hzuapps.androidlabs.soft1714080902228;
+package soft1714080902228.androidlabs.hzuapps.edu.soft1714080902228;
 
-public class Soft1714080902228Activity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1714080902228Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902228);
+    }
 }
