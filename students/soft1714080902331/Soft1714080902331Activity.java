1dbb4790318b8ac264632309e0ea208a3a6895cf
XIAODINGD <850243182@qq.com>
2019-03-15 19:51:48 +0800

    实验二代码

---
+++ students/soft1714080902331/Soft1714080902331Activity.java
@@ -0,0 +1,19 @@
+package edu.hzuapps.androidlabs.soft1714080902331;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class Soft1714080902331Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902331_activity);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
