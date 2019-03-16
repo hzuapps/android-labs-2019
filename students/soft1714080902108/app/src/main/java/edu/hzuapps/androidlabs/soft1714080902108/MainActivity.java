63a1335c0968b321b4e9415219ae6ff23c9fdda4
fanlihuang <1633289851@qq.com>
2019-03-16 12:18:12 +0800

    提交第二次代码

---
+++ students/soft1714080902108/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902108/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.androidlabs.soft1714080902108;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.myapplication2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft1714080902108activity_main);
+    }
+}
