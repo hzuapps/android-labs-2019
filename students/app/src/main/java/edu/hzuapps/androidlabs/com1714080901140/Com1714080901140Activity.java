55faf240da585acf4e2d6cfd6d0df4d5357f7063
liebeyiyi <40763965+liebeyiyi@users.noreply.github.com>
2019-03-16 14:15:04 +0800

    #2

---
+++ students/app/src/main/java/edu/hzuapps/androidlabs/com1714080901140/Com1714080901140Activity.java
@@ -0,0 +1,9 @@
+package edu.hzuapps.androidlabs.com1714080901140;
+s
+public class Com1714080901140Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1714080901112);
+    }
+}
