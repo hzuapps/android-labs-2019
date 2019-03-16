018b926afda0ac67abc468d603db0ec019f05dbc
harrypote <214595932@qq.com>
2019-03-16 17:00:28 +0800

    #2 #733 实验二“

---
+++ students/soft1709081602219/app/src/main/java/com/edu/hzuapps/androidlabs/Soft1709081602219/soft1709081602219Activity.java
@@ -0,0 +1,13 @@
+package com.edu.hzuapps.androidlabs.Soft1709081602219;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class soft1709081602219Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1709081602219_activity);
+    }
+}
