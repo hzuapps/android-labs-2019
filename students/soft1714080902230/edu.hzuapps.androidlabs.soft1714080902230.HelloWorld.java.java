862d9f7f270dafc566651180c0503f4db6b43c76
alicekagiyama <48143965+alicekagiyama@users.noreply.github.com>
2019-03-16 09:48:46 +0800

    Delete edu.hzuapps.androidlabs.soft1714080902230.HelloWorld.java.java

--- students/soft1714080902230/edu.hzuapps.androidlabs.soft1714080902230.HelloWorld.java.java
+++
@@ -1,8 +0,0 @@
-package package edu.hzuapps.androidlabs.soft1714080902230;
-
-public class Soft1714080902230Activity {
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
-	}
-}
