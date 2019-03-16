dffe5dd34455d643d4544373fd710aba9fef45a5
Tohsakaemmmmm <1036581917@qq.com>
2019-03-17 00:21:12 +0800

    第二次试验

---
+++ students/soft1714080902211/main/java/edu/hzuapps/androidlabs/soft1714080902211/soft1714080902211jieriFragment.java
@@ -0,0 +1,22 @@
+package edu.hzuapps.androidlabs.soft1714080902211;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class soft1714080902211jieriFragment extends Fragment {
+
+    public soft1714080902211jieriFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_soft1714080902211jieri, container, false);
+    }
+}
