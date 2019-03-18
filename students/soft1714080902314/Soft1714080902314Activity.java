4e2f6965c40cd47f44d22e6685b6a7a2c4dbb5a5
yanghuim <48150197+yanghuim@users.noreply.github.com>
2019-03-18 13:42:22 +0800

    #2 #补交实验代码

---
+++ students/soft1714080902314/Soft1714080902314Activity.java
@@ -0,0 +1,11 @@
+package edu.hzuapps.androidlabs.soft1714080902314;
+ 
+   public class Soft1714080902314Activity{ 
+
+    public static void main(String[] args){
+ 
+    System.out.println("Hello World!")
+ 
+    } 
+
+ } 
