62069ca34810e713044a32d8518546ec68248d0b
Bswordload <1292690617@qq.com>
2019-03-19 15:28:29 +0800

    #3 Bswordload 第3次实验

--- students/soft1714080902202/第三次实验/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902202/soft1714080902202MainPageActivity.java
+++ students/soft1714080902202/第三次实验/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902202/soft1714080902202MainPageActivity.java
@@ -5,9 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class soft1714080902202MainPageActivity extends AppCompatActivity {
+public class Soft1714080902202MainPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +17,7 @@ public class soft1714080902202MainPageActivity extends AppCompatActivity {
         btnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(soft1714080902202MainPageActivity.this,soft1714080902202timer.class));
+                startActivity(new Intent(Soft1714080902202MainPageActivity.this, Soft1714080902202timer.class));
             }
         });
    }
--- students/soft1714080902202/第三次实验/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902202/soft1714080902202timer.java
+++ students/soft1714080902202/第三次实验/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902202/soft1714080902202timer.java
@@ -3,7 +3,7 @@ package edu.hzuapps.androidlabs.soft1714080902202;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class soft1714080902202timer extends AppCompatActivity {
+public class Soft1714080902202timer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
