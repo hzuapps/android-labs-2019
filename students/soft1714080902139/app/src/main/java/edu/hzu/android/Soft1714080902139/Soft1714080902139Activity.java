7764fb6303147b6a7f58c43833b678911d98d395
975256949 <975256949@qq.com>
2019-03-21 00:33:17 +0800

    实验三

--- students/soft1714080902139/app/src/main/java/edu/hzu/android/Soft1714080902139/Soft1714080902139Activity.java
+++ students/soft1714080902139/app/src/main/java/edu/hzu/android/Soft1714080902139/Soft1714080902139Activity.java
@@ -1,37 +1,24 @@
-package edu.hzu.android.Soft1714080902139;
+package edu.hzu.android.soft1714080902139;
 
-        import android.annotation.SuppressLint;
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
-        import android.view.KeyEvent;
-        import android.view.MotionEvent;
-        import android.widget.Toast;
+        import android.view.View;
 
 public class Soft1714080902139Activity extends AppCompatActivity {
+
+    private View startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1714080902139_activity);
     }
-    @SuppressLint("WrongConstant")
-    public boolean onKeyDown(int keyCode, KeyEvent event){
-        Toast.makeText(this,"按键按下！",0).show();
-        return super.onKeyUp(keyCode, event);
-    }
 
-    @SuppressLint("WrongConstant")
-    public boolean onKeyUp(int keyCode, KeyEvent event){
-        Toast.makeText(this,"按键弹起！",0).show();
-        return  super.onKeyUp(keyCode, event);
-    }
-
-    @SuppressLint("WrongConstant")
-    public boolean onTouchEvent(MotionEvent event){
-        float x=event.getX();
-        float y=event.getY();
-        Toast.makeText(this,"点击的坐标为（"+x+"："+y+"）",0).show();
-        return  super.onTouchEvent(event);
+    public void skip(View view){
+        Intent intent = new Intent();
+        intent.setClass(Soft1714080902139Activity.this,Soft1714080902139Activity2.class);
+        startActivity(intent);
     }
 
 
