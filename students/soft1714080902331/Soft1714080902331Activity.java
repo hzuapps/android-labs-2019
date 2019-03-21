eceafc02591e0b510f81e0e2a7b11e84749888e7
XIAODINGD <850243182@qq.com>
2019-03-21 22:34:54 +0800

    实验三代码

--- students/soft1714080902331/Soft1714080902331Activity.java
+++ students/soft1714080902331/Soft1714080902331Activity.java
@@ -1,8 +1,13 @@
 package edu.hzuapps.androidlabs.soft1714080902331;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class Soft1714080902331Activity extends AppCompatActivity {
 
@@ -10,10 +15,20 @@ public class Soft1714080902331Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1714080902331_activity);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
+        TextView btnOpen = (TextView) findViewById(R.id.textview_01);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Soft1714080902331Activity.this,Soft1714080902331Activity2.class));
+            }
+        });
+        TextView btnOpen2 = (TextView) findViewById(R.id.textview_02);
+        btnOpen2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Soft1714080902331Activity.this,Soft1714080902331Activity3.class));
+            }
+        });
     }
 
 }
---
+++ students/soft1714080902331/Soft1714080902331Activity3.java
@@ -0,0 +1,19 @@
+package edu.hzuapps.androidlabs.soft1714080902331;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Soft1714080902331Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902331_activity3);
+
+    }
+
+}
