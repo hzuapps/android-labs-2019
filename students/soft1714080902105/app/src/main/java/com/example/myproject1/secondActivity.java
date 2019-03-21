5dadc87c511d69b6e1fdf7f0e895c4b1653ba984
correnter <1164443205@qq.com>
2019-03-21 22:52:25 +0800

    实验三

--- students/soft1714080902105/app/src/main/java/com/example/myproject1/secondActivity.java
+++ students/soft1714080902105/app/src/main/java/com/example/myproject1/secondActivity.java
@@ -1,13 +1,16 @@
-package com.example.soft1714080902105activity;
+package com.example.myproject1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+public class secondActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
+
     }
 }
