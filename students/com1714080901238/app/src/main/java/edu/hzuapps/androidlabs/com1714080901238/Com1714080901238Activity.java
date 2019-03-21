29770febb53e105b2b9e44416fa7adf200b27d5c
wyj-2019 <957534632@qq.com>
2019-03-22 00:15:03 +0800

    #3 #473 #第三次实验

--- students/com1714080901238/app/src/main/java/edu/hzuapps/androidlabs/com1714080901238/Com1714080901238Activity.java
+++ students/com1714080901238/app/src/main/java/edu/hzuapps/androidlabs/com1714080901238/Com1714080901238Activity.java
@@ -1,5 +1,6 @@
-package edu.hzuapps.com1714080901238;
+package edu.hzuapps.androidlabs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import edu.hzuapps.androidlabs.R;
 
 public class Com1714080901238Activity extends AppCompatActivity {
 
@@ -24,8 +23,8 @@ public class Com1714080901238Activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(Com1714080901238Activity.this,Com1714080901238Activity02.class);
+                startActivity(intent);
             }
         });
     }
