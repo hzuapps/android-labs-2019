380b3fc4eee3b521d239ba9e45d0096110dd98a3
jieers <774685879@qq.com>
2019-03-21 23:04:34 +0800

    第三次实验

--- students/soft1714080902204/soft1714080902204Activity.java
+++ students/soft1714080902204/soft1714080902204Activity.java
@@ -1,29 +1,25 @@
 package edu.hzuapps.androidlabs.soft1714080902204;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Soft1714080902204Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.soft_1714080902204_activity);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_soft1714080902204);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button btnOpen = findViewById(R.id.button);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(Soft1714080902204Activity.this,Soft1714080902204Activity_Second.class));
+
             }
         });
     }
-
 }
