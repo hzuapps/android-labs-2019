0e715c884f40f0c2d4358503316a6792dc8b91fb
RvingL <944251366@qq.com>
2019-03-21 22:48:53 +0800

    实验三

--- students/soft1714080902217/edu/hzuapps/androidlabs/Soft1714080902217/Soft1714080902217Activity.java
+++ students/soft1714080902217/edu/hzuapps/androidlabs/Soft1714080902217/Soft1714080902217Activity.java
@@ -1,9 +1,10 @@
 package edu.hzuapps.androidlabs.soft1714080902217;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.example.myapplication.R;
+import android.view.View;
+import android.widget.Button;
 
 public class Soft1714080902217Activity extends AppCompatActivity {
 
@@ -11,5 +12,12 @@ public class Soft1714080902217Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1714080902217);
+        Button btnOpen = findViewById(R.id.button);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Soft1714080902217Activity.this, Soft1714080902217Activity_1.class));
+            }
+        });
     }
 }
