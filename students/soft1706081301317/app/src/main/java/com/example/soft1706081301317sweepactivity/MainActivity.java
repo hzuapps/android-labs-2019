a8b69dd9829b5e79a2ad1bf6f2e1ac30cbb51721
cqr22 <1309382951@qq.com>
2019-03-21 23:37:00 +0800

    实验3

--- students/soft1706081301317/app/src/main/java/com/example/soft1706081301317sweepactivity/MainActivity.java
+++ students/soft1706081301317/app/src/main/java/com/example/soft1706081301317sweepactivity/MainActivity.java
@@ -1,18 +1,18 @@
-package com.example.soft1706081301317sweepactivity;
+package com.example.soft1706081301317sweepacticity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.example.soft1706081301317sweepactivity.R;
-
-public class Soft1706081301317SweepActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +21,13 @@ public class Soft1706081301317SweepActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        TextView textView = (TextView) findViewById(R.id.begin);
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SweepActicity.class);
+                startActivity(intent);
+
             }
         });
     }
@@ -52,5 +53,4 @@ public class Soft1706081301317SweepActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
