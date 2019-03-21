0f2d42e1180764a50740718b5a248ffdb02f2486
1714080902306 <1240780563@qq.com>
2019-03-21 15:01:12 +0800

    #3 #1714080902306 第三次实验

---
+++ students/soft1714080902306/ShiYan03/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902306/Soft1714080902306Activity01.java
@@ -0,0 +1,28 @@
+package edu.hzuapps.androidlabs.soft1714080902306;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import edu.hzuapps.androidlabs.R;
+
+public class Soft1714080902306Activity01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902306_activity01);
+
+        TextView btnOpen = (TextView) findViewById(R.id.textview_01);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Soft1714080902306Activity01.this,Soft1714080902306Activity02.class));
+            }
+        });
+    }
+
+}
