48ceb4a1a5dc4e8247e16c1e820af54938a5d988
cxh245 <245385661@qq.com>
2019-03-22 01:05:21 +0800

    第三次实验-家教管理系统

--- students/com1714080901108/Com1714080901108Activity.java
+++
@@ -1,5 +0,0 @@
-package edu.hzuapps.androidlabs.com1714080901108;
-
-public class Com1714080901108Activity {
-
-}
--- students/com1714080901108/app/src/main/java/edu/hzuapps/androidlabs/com1714080901108/Com1714080901108Activity.java
+++ students/com1714080901108/app/src/main/java/edu/hzuapps/androidlabs/com1714080901108/Com1714080901108Activity.java
@@ -1,29 +1,27 @@
 package edu.hzuapps.androidlabs.com1714080901108;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class Com1714080901108Activity extends AppCompatActivity {
 
+public class Com1714080901108Activity extends AppCompatActivity implements View.OnClickListener{
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com1714080901108);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Button button=(Button) findViewById(R.id.button);
+        imageView=(ImageView) findViewById(R.id.img_list);
+        button.setOnClickListener(this);
     }
 
+        @Override
+    public void onClick(View view) {
+        Intent intent=new Intent(Com1714080901108Activity.this,SecondActivity.class);
+        startActivity(intent);
+}
 }
