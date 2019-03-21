a06fed07f9209ddd1b5d3ed02fcd354386a70d51
CAIHAOXIN <1149227370@qq.com>
2019-03-21 14:02:46 +0800

    实验三代码

--- students/soft1714080902125/java/edu/hzuapps/androidlabs/soft1714080902125/Soft1714080902125Activity.java
+++ students/soft1714080902125/java/edu/hzuapps/androidlabs/soft1714080902125/Soft1714080902125Activity.java
@@ -1,15 +1,37 @@
 package edu.hzuapps.androidlabs.soft1714080902125;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.helloworldactivity.R;
 
 public class Soft1714080902125Activity extends AppCompatActivity {
 
+    private Button b1;
+    private Button b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_soft1714080902125);
+        setContentView(R.layout.soft_1714080902125_activity);
+        b1=findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Soft1714080902125Activity.this,greenbook.class);
+                startActivity(intent);
+            }
+        });
+        b2=findViewById(R.id.b2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Soft1714080902125Activity.this,shawshank.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
