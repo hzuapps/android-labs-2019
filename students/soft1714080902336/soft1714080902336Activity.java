1c40d47b250e98f9b15ae8d8e10346da7aaeab3c
tianyidage <1184837567@qq.com>
2019-03-21 20:48:21 +0800

    #3 #275 第三次实验

--- students/soft1714080902336/soft1714080902336Activity.java
+++ students/soft1714080902336/soft1714080902336Activity.java
@@ -1,13 +1,28 @@
-package com.example.soft1714080902336;
+package edu.hzuapps.androidlabs.soft1714080902336;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Soft1714080902336Activity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.soft_1714080902336_activity);
+        setContentView(R.layout.activity_soft1714080902336);
+
+        textView=(TextView)findViewById(R.id.textview01);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Soft1714080902336Activity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
