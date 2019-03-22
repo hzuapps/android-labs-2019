214e3f653cfc3c4e29e7c68088a54c30a53c6adf
hongxiujie <903037093@qq.com>
2019-03-22 21:43:43 +0800

    #3 提交实验3代码

--- students/com1714080901124/app/src/main/java/edu/hzuapps/androidlabs/com1714080901124/Com1714080901124Activity.java
+++ students/com1714080901124/app/src/main/java/edu/hzuapps/androidlabs/com1714080901124/Com1714080901124Activity.java
@@ -1,7 +1,10 @@
-package edu.hzuapps.androidlabs.com1714080901124;
+package edu.hzuapps.androidlabs.test3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Com1714080901124Activity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Com1714080901124Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.com_1714080901124_activity);
+        TextView text=findViewById(R.id.textView);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Com1714080901124Activity.this,Com1714080901124Activity2.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+    }
+
