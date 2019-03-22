fe7ef65eff5224caefd42a4ca97a7ce2697ecf48
anxiangleisuhen <1498390998@qq.com>
2019-03-22 13:31:49 +0800

    #3 第三次实验

---
+++ students/soft1714080902225/实验三/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902225/Soft1714080902225_1.java
@@ -0,0 +1,24 @@
+package edu.hzuapps.androidlabs.soft1714080902225;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Soft1714080902225_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902225_1_activity);
+        View button = findViewById(R.id.mai);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(Soft1714080902225_1.this, Soft1714080902225_2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
