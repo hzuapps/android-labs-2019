f7f350339a34f7a03a6908772d2f182d6045c4b5
SunJiashun <316218906@qq.com>
2019-03-22 18:51:30 +0800

    #3 #SunJiashun 第三次实验

---
+++ students/soft1509081602625/app/src/main/java/edu/hzu/soft1509081602625/CarInfo.java
@@ -0,0 +1,12 @@
+package edu.hzu.soft1509081602625;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CarInfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.carinfo_soft1509081602625);
+    }
+}
