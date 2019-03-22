d2884dc3ef072663d45f0423bb810384f160aded
tianyidage <1184837567@qq.com>
2019-03-22 20:12:18 +0800

    #2 #275 第二次实验

---
+++ students/soft1714080902336/shiyan2/Soft1714080902336Activity.java
@@ -0,0 +1,13 @@
+package com.example.soft1714080902336;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1714080902336Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902336_activity);
+    }
+}
