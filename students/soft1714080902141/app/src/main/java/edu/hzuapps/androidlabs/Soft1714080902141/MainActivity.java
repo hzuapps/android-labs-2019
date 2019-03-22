7880fe51f0bf72520a3b2cc9057ebcb8d736d1f5
1714080902141 <1321380131@qq.com>
2019-03-23 07:40:47 +0800

    实验三

---
+++ students/soft1714080902141/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902141/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.a123456);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
