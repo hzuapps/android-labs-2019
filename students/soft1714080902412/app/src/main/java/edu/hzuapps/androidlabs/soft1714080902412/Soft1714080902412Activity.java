361f67e8bd36c7f087102f3444c215586eb608ae
HeWeiNan1 <1052972240@11.com>
2019-03-22 19:48:49 +0800

    #3 #HeWeiNan1 第3次实验

--- students/soft1714080902412/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902412/Soft1714080902412Activity.java
+++ students/soft1714080902412/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902412/Soft1714080902412Activity.java
@@ -1,24 +1,32 @@
 package edu.hzuapps.androidlabs.soft1714080902412;
+
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class Soft1714080902412Activity extends AppCompatActivity implements View.OnClickListener {
+public class Soft1714080902412Activity extends AppCompatActivity implements View.OnClickListener{
+   private Button button;
 
-    private Button button;
+   private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1714080902412);
-        button=(Button)findViewById(R.id.button);
+        button=findViewById(R.id.button1);
         button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent =new Intent(Soft1714080902412Activity.this,Soft1714080902412Activity2.class);
-        startActivity(intent);
+        Intent intent=new Intent(Soft1714080902412Activity.this,Soft1714080902412Activity_2.class);
+         startActivity(intent);
     }
 }
