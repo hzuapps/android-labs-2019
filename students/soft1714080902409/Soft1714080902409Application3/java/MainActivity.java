59cfbf58b45abadbde146e68691d8965ea11f9fc
liutao2019 <1980661451@qq.com>
2019-03-22 22:07:10 +0800

    #3 #第三次实验

---
+++ students/soft1714080902409/Soft1714080902409Application3/java/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changetosend(View view){
+        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+        startActivity(intent);
+    }
+
+}
