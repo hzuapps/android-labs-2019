389627f51b860a4932f067866e57d99ed6a2a8e6
1085852964 <1085852964@qq.com>
2019-03-23 00:32:33 +0800

    update

--- students/soft1709081602513/app/src/main/java/edu/hzuapps/androidlabs/soft1709081602513/Soft1709081602513Activity.java
+++ students/soft1709081602513/app/src/main/java/edu/hzuapps/androidlabs/soft1709081602513/Soft1709081602513Activity.java
@@ -3,12 +3,11 @@ package edu.hzuapps.androidlabs.soft1709081602513;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Soft1709081602513Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_soft2);
     }
 }
--- students/soft1714080902214/Soft1714080902214Activity.java
+++
@@ -1,7 +0,0 @@
-package edu.hzuapps.androidlabs.soft1714080902214;
-
-public class Soft1714080902214Activity {
-    public static void main(String args[]){
-        System.out.println("Hello World!");
-    }
-}
