d7386e60c23be8dfdb7757a00e959af20f62a4ce
305zhengzesheng <1342561947@qq.com>
2019-03-22 20:56:05 +0800

    第三次实验

---
+++ students/soft1714080902305/mythirdapp/src/main/java/edu/hzuapps/androidlabs/soft1714080902305/Soft1714080902305.java
@@ -0,0 +1,55 @@
+package edu.hzuapps.androidlabs.soft1714080902305;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class Soft1714080902305 extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private TextView cxMessage;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902305);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        cxMessage = (TextView) findViewById(R.id.textview_01);
+        cxMessage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Soft1714080902305.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
