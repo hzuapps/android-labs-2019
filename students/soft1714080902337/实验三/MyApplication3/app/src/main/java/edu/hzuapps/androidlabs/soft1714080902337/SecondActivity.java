378c020fc1e82538421fec984c7b9df72c05b0c5
= <=>
2019-03-22 21:38:55 +0800

    第三个文件

---
+++ students/soft1714080902337/实验三/MyApplication3/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902337/SecondActivity.java
@@ -0,0 +1,25 @@
+package edu.hzuapps.androidlabs.soft1714080902337;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import edus.huzapp.androidlabs.soft1714080902337.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+    }
+}
