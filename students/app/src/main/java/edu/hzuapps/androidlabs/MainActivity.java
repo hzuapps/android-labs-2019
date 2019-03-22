2fe665342af9e891c503a4496edf4ed6481dbd88
leexwen <1339888671@qq.com>
2019-03-22 21:14:58 +0800

    提交实验三

---
+++ students/app/src/main/java/edu/hzuapps/androidlabs/MainActivity.java
@@ -0,0 +1,41 @@
+package edu.hzuapps.androidlabs;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView =(TextView)findViewById(R.id.text);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, Soft1714080902437Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView imageView;
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Soft1714080902437Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
