e9344ef699c51eb4218bf4e594c9cbd27cb441cd
AngleBeatQAQ <499352824@qq.com>
2019-03-22 22:01:39 +0800

    #3 #第三次实验

---
+++ students/soft1714080902425/实验三/java/MainActivity.java
@@ -0,0 +1,28 @@
+package edu.androidlabs.thirdtest;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button test_btn=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        test_btn=(Button) findViewById(R.id.btn1);
+        test_btn.setOnClickListener(new MainActivity.MyButtonListener());
+    }
+    private class MyButtonListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            Intent intent = new Intent(MainActivity.this,MusicFileNameFilter.class);
+            startActivity(intent);
+        }
+    }
+}
