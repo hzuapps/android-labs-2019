13a628ad299fca92e20fa6284e311eb211ae2f3a
HurryTomcat <48144217+HurryTomcat@users.noreply.github.com>
2019-03-22 12:45:21 +0800

    Add files via upload

---
+++ students/Soft1714080902215/问卷调查系统-实验3/java/edu/hzuapps/androidlabs/Soft1714080902215/pic.java
@@ -0,0 +1,26 @@
+package edu.hzuapps.androidlabs.Soft1714080902215;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class pic extends AppCompatActivity {
+    private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pic);
+
+        button2 = (Button) findViewById(R.id.button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(edu.hzuapps.androidlabs.Soft1714080902215.pic.this, Soft1714080902215Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
