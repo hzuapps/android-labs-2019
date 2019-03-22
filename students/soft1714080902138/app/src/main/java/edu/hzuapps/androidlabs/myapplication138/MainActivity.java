b598761d3c2fd7f4614892d50052640347381660
woshigaoshou <1258006989@qq.com>
2019-03-23 01:09:28 +0800

    第3次实验

--- students/soft1714080902138/app/src/main/java/edu/hzuapps/androidlabs/myapplication138/MainActivity.java
+++ students/soft1714080902138/app/src/main/java/edu/hzuapps/androidlabs/myapplication138/MainActivity.java
@@ -1,13 +1,16 @@
-package edu.hzuapps.androidlabs.soft1714080902138;
+package edu.hzuapps.androidlabs.myapplication138;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Soft1714080902138Activity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.soft_1714080902138_activity);
+        setContentView(R.layout.activity_main);
     }
 }
