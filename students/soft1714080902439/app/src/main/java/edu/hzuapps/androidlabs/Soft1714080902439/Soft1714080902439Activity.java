bfde1db2771e5867f9ed7a89056b328336325020
1714080902439 <741131502@qq.com>
2019-03-23 20:51:16 +0800

    实验三

--- students/soft1714080902439/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902439/Soft1714080902439Activity.java
+++ students/soft1714080902439/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902439/Soft1714080902439Activity.java
@@ -1,22 +1,27 @@
-package edu.hzuapps.androidlabs.Soft1714080902439;
+package edu.hzuapps.androidlabs.soft1714080902439;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.soft171408092439.R;
 
+
 public class Soft1714080902439Activity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1714080902439_activity);
+        button=(Button) findViewById(R.id.button);
+        //按钮绑定接口
     }
-    //添加生日
+
+    /*//添加生日
     @SuppressLint("WrongConstant")
     public boolean onKeyDown(int keyCode, KeyEvent event)   {
         Toast.makeText(this,"123" ,0).show();
@@ -30,4 +35,5 @@ public class Soft1714080902439Activity extends AppCompatActivity {
         Toast.makeText(this,"Service unavaliable!",0).show();
         return super.onTouchEvent(event);
     }
+*/
 }
---
+++ students/soft1714080902439/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902439/Soft1714080902439ImageActivity.java
@@ -0,0 +1,32 @@
+package edu.hzuapps.androidlabs.soft1714080902439;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.soft171408092439.R;
+
+public class Soft1714080902439ImageActivity extends AppCompatActivity{
+    private TextView textView;
+    //private  Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902439_image_activity);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Toast text=Toast.makeText(edu.hzuapps.androidlabs.soft1714080902439.Soft1714080902439ImageActivity.this,"123",Toast.LENGTH_SHORT);
+                // 另一个Activity的完整名称 = edu.androidlabs.soft123456(包名小写).Soft123456Activity(类名)
+                Intent intent = new Intent(edu.hzuapps.androidlabs.soft1714080902439.Soft1714080902439ImageActivity.this,edu.hzuapps.androidlabs.soft1714080902439.Soft1714080902439Activity.class);
+                startActivity(intent);
+                text.show();
+
+            }
+        });
+    }
+}
