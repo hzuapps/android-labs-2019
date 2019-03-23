6da96c3c820d6136aa5e733b9f7d496362988bb2
Hzu-sick <1312203573@qq.com>
2019-03-23 15:05:34 +0800

    实验2修改

--- students/Soft1712070504232/java/edu/hzuapps/androidlabs/Soft1712070504232/Soft1712070504232Activity.java
+++ students/Soft1712070504232/java/edu/hzuapps/androidlabs/Soft1712070504232/Soft1712070504232Activity.java
@@ -1,24 +1,12 @@
 package edu.hzuapps.androidlabs.soft1712070504232;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class Soft1712070504232Activity extends Activity {
-    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1712070504232_activity);
-        textView = (TextView) findViewById(R.id.phone);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Soft1712070504232Activity.this, Soft1712070504232Activity2.class);
-                startActivity(intent);
-            }
-        });
     }
 }
