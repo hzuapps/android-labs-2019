53941f5242cef7a8f621b9c7c169728b8c1a64fe
1372037590 <1372037590@qq.com>
2019-03-23 19:21:07 +0800

    #3 实验三代码

--- students/com1714080901115/app/src/main/java/edu/hzuapps/androidlabs/com1714080901115/Com1714080901115Activity.java
+++ students/com1714080901115/app/src/main/java/edu/hzuapps/androidlabs/com1714080901115/Com1714080901115Activity.java
@@ -4,16 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 public class Com1714080901115Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.com_1714080901115_activity);
-        Button denglu = findViewById(R.id.button1);
+        TextView denglu = findViewById(R.id.textView4);
        denglu.setOnClickListener (new View.OnClickListener() {
             @Override
             public void onClick(View v) {
