d8f11057df1592d149ffcc180d3c40061aa3a330
hsl401dcb <594391417@qq.com>
2019-03-23 22:34:24 +0800

    #3 第3次实验

---
+++ students/soft1709081602231/java/edu/hzuapps/androidlabs/soft1709081602231/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.soft1709081602231;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
