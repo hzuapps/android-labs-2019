552585def386bab620872f2874c8db9e8aef4b1c
KasenBob <826107692@qq.com>
2019-03-24 21:10:00 +0800

    #第四次实验 KasenBob

--- students/soft1714080902216/edu/hzuapps/androidlabs/soft1714080902216/Soft1714080902216RecordActivity.java
+++ students/soft1714080902216/edu/hzuapps/androidlabs/soft1714080902216/Soft1714080902216RecordActivity.java
@@ -1,4 +1,40 @@
 package edu.hzuapps.androidlabs.soft1714080902216;
 
-public class Soft1714080902216RecordActivity {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import static edu.hzuapps.androidlabs.soft1714080902216.R.*;
+
+public class Soft1714080902216RecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902216_record);
+        Button inputLogin = (Button) findViewById(R.id.inputLogin);
+        Button Comeback = (Button) findViewById(R.id.Comeback);
+        inputLogin.setOnClickListener(new Soft1714080902216RecordActivity.MyButton());
+        Comeback.setOnClickListener(new Soft1714080902216RecordActivity.MyButton());
+    }
+
+    private class MyButton implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            switch (v.getId()) {
+                case R.id.inputLogin:
+                    break;
+                case R.id.Comeback:
+                    intent = new Intent(Soft1714080902216RecordActivity.this, Soft1714080902216MainActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    }
 }
+
+
--- students/soft1714080902216/edu/hzuapps/androidlabs/soft1714080902216/Soft1714080902216UserActivity.java
+++ students/soft1714080902216/edu/hzuapps/androidlabs/soft1714080902216/Soft1714080902216UserActivity.java
@@ -1,7 +1,11 @@
 package edu.hzuapps.androidlabs.soft1714080902216;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Soft1714080902216UserActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class Soft1714080902216UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1714080902216_user);
+        Button denglu=(Button) findViewById(R.id.denglu);
+        Button zhuce= (Button) findViewById(R.id.zhuce);
+        Button back=(Button) findViewById(R.id.back);
+        denglu.setOnClickListener(new Soft1714080902216UserActivity.myButton());
+        zhuce.setOnClickListener(new Soft1714080902216UserActivity.myButton());
+        back.setOnClickListener(new Soft1714080902216UserActivity.myButton());
+    }
+
+    private class myButton implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v)
+        {
+            Intent intent;
+            switch(v.getId())
+            {
+                case R.id.button1:
+                    break;
+                case R.id.button2:
+                    break;
+                case R.id.imageButton:
+                    intent=new Intent(Soft1714080902216UserActivity.this,Soft1714080902216MainActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
     }
 }
