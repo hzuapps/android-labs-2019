a738880fa64d83df7d2d347d6f1c99e0206634c8
hcwcc <1497330792@qq.com>
2019-03-26 00:27:20 +0800

    #3 #hcwcc 第三次实验

--- students/soft1714080902419/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902419/Soft1714080902419Activity.java
+++ students/soft1714080902419/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902419/Soft1714080902419Activity.java
@@ -1,13 +1,26 @@
 package edu.hzuapps.androidlabs.soft1714080902419;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Soft1714080902419Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_soft1714080902419);
+
+        Button GoButton = (Button) findViewById(R.id.button);
+        GoButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Soft1714080902419Activity.this,Soft1714080902419pikaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
