169aa1fef5d60ab263d3e5e10db87bcbef955f73
= <=>
2019-03-26 09:30:49 +0800

    第二个文件

---
+++ students/soft1714080902337/实验2/src/main/java/edu/hzuapps/androidlabs/soft1714080902337/Soft1714080902337Activity.java
@@ -0,0 +1,16 @@
+package edu.hzuapps.androidlabs.soft1714080902337;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+
+public class Soft1714080902337Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902337);
+    }
+}
