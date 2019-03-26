4ac00633763308bb1975945673dde89dd9c4c720
1714080902439 <741131502@qq.com>
2019-03-26 18:47:59 +0800

    生日管家第三次实验

--- students/soft1714080902439/Soft1714080902439Activity.java
+++
@@ -1,7 +0,0 @@
-package com.example.soft171408092439.edu.hzuapps.androidlabs.soft1714080902439;
-
-public class Soft1714080902439Activity {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld!");
-    }
-}
