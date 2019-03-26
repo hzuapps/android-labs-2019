49b759acb85e35089b6d35bc350292be7d12dd14
zhuanglinwu <342663636@qq.com>
2019-03-26 21:44:25 +0800

    #268 第四次试验

--- students/soft1714080902113/app/src/main/java/com/example/soft1714080902113myexpress/SendExpress.java
+++ students/soft1714080902113/app/src/main/java/com/example/soft1714080902113myexpress/SendExpress.java
@@ -3,17 +3,16 @@ package com.example.soft1714080902113myexpress;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-public class MyInfo extends AppCompatActivity {
+
+public class SendExpress extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_info);
+        setContentView(R.layout.activity_send_express);
         ActionBar actionBar=getSupportActionBar();
         if(actionBar!=null){
             actionBar.hide();
         }
     }
-
-
 }
