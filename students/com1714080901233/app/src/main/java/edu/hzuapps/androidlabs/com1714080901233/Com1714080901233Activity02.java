880cef532dee8ae62c77e216cbfaec9010186ecd
wechat210 <2476205143@qq.com>
2019-03-27 23:25:45 +0800

    #4 #315 #第4次实验

--- students/com1714080901233/app/src/main/java/edu/hzuapps/androidlabs/com1714080901233/Com1714080901233Activity02.java
+++ students/com1714080901233/app/src/main/java/edu/hzuapps/androidlabs/com1714080901233/Com1714080901233Activity02.java
@@ -1,12 +1,24 @@
 package edu.hzuapps.androidlabs.com1714080901233;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Com1714080901233Activity02 extends Activity {
+    private Button button6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.com_1714080901233_activity_02);
+        button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Com1714080901233Activity02.this, Com1714080901233Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
