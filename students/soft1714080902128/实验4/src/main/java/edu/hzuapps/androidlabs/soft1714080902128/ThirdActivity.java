1f3cb866c2d80874573159a8cf3f2651df9eed40
zxc9711 <971165874@qq.com>
2019-03-27 10:42:32 +0800

    update lab4

---
+++ students/soft1714080902128/实验4/src/main/java/edu/hzuapps/androidlabs/soft1714080902128/ThirdActivity.java
@@ -0,0 +1,12 @@
+package edu.hzuapps.androidlabs.soft1714080902128;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_activity);
+    }
+}
