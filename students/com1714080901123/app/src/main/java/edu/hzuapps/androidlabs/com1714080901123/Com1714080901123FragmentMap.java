5ca63807eb96fc9f26fea75f688814cd027dd0e1
ZedCxs <847807307@qq.com>
2019-03-28 00:33:18 +0800

    提交实验四代码

---
+++ students/com1714080901123/app/src/main/java/edu/hzuapps/androidlabs/com1714080901123/Com1714080901123FragmentMap.java
@@ -0,0 +1,23 @@
+package edu.hzuapps.androidlabs.com1714080901123;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+public class Com1714080901123FragmentMap extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_com1714080901123_fragment_map, container, false);
+    }
+}
