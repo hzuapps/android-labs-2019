86f30b757a0bb342967741014f7528b9d3446167
xuxiaoting1128 <524269667@qq.com>
2019-03-29 00:24:48 +0800

    #4实验四代码

--- students/com1712070504104/app/src/main/java/edu/hzuapps/androidlabs/com1712070504104/Com1712070504104Activity.java
+++ students/com1712070504104/app/src/main/java/edu/hzuapps/androidlabs/com1712070504104/Com1712070504104Activity.java
@@ -1,10 +1,12 @@
 package edu.hzuapps.androidlabs.com1712070504104;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
 
 
 public class Com1712070504104Activity extends AppCompatActivity {
@@ -13,14 +15,15 @@ public class Com1712070504104Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.com_1712070504104_activity);
-        TextView text=findViewById(R.id.textView2);
-        text.setOnClickListener(new View.OnClickListener() {
+        TextView text=findViewById(R.id.app_name);
+        text.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Com1712070504104Activity.this,Com1712070504104Activity2.class);
                 startActivity(intent);
             }
         });
+
     }
 
 }
--- students/com1712070504104/app/src/main/java/edu/hzuapps/androidlabs/com1712070504104/Com1712070504104Activity2.java
+++ students/com1712070504104/app/src/main/java/edu/hzuapps/androidlabs/com1712070504104/Com1712070504104Activity2.java
@@ -3,11 +3,14 @@ package edu.hzuapps.androidlabs.com1712070504104;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.myapplication.R;
+
 public class Com1712070504104Activity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.com_17120705041042_activity2);
+        setContentView(R.layout.com_1712070504104_activity2);
     }
-            }
+}
+
