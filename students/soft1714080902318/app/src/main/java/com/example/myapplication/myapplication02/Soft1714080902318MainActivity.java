9bf79f311076f53b33afebef56c6521118a2c1ce
ReiujiTenshi <592130546@q.com>
2019-03-29 01:57:14 +0800

    提交实验3代码

--- students/soft1714080902318/app/src/main/java/com/example/myapplication/myapplication02/Soft1714080902318MainActivity.java
+++ students/soft1714080902318/app/src/main/java/com/example/myapplication/myapplication02/Soft1714080902318MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.myapplication.myapplication02;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class Soft1714080902318MainActivity extends AppCompatActivity {
@@ -39,6 +41,14 @@ public class Soft1714080902318MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mTextMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Soft1714080902318MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
