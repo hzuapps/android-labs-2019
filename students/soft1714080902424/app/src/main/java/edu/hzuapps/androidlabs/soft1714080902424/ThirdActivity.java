9935f0b48f3c91f06626f8de3365fe26f037595b
Kowaiking <382894648@qq.com>
2019-03-28 13:29:05 +0800

    #4 #Kowaiking 第4次实验

--- students/soft1714080902424/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902424/ThirdActivity.java
+++ students/soft1714080902424/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902424/ThirdActivity.java
@@ -2,15 +2,12 @@ package edu.hzuapps.androidlabs.soft1714080902424;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
-    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_third);
     }
-
 }
