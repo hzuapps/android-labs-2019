48fbc6a2d4e900a944c37c1b9f1b538f05c34fc6
LamKP <78664376@qq.com>
2019-03-28 22:09:09 +0800

    Exp4_HR管理系统

--- students/soft1714080902209/HR管理系统/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902209_Login.java
+++ students/soft1714080902209/HR管理系统/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902209_Login.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class Soft1714080902209_Login extends AppCompatActivity {
@@ -16,15 +19,20 @@ public class Soft1714080902209_Login extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            String[] data={"Apple","Banana","Orange","Watermelon","Pear","Grape","Pineapple","Strawberry","Cherry","Mango","Apple","Banana","Orange","Watermelon","Pear","Grape","Pineapple","Strawberry","Cherry","Mango"};
+            ArrayAdapter<String> adapter=new ArrayAdapter<String>(Soft1714080902209_Login.this,android.R.layout.simple_list_item_1,data);
+            ListView listview=(ListView)findViewById(R.id.list_view);
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    listview.setAdapter(adapter);
+                    listview.setVisibility(View.VISIBLE);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    listview.setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    listview.setVisibility(View.INVISIBLE);
                     return true;
             }
             return false;
@@ -34,7 +42,7 @@ public class Soft1714080902209_Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.soft1714080902209__login);
+        setContentView(R.layout.soft1714080902209_login);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
