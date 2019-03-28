37ac7254c10b4ec7ab46590195b128e459594230
XIAODINGD <850243182@qq.com>
2019-03-28 15:31:42 +0800

    实验四代码

--- students/soft1714080902331/Soft1714080902331Activity2.java
+++ students/soft1714080902331/Soft1714080902331Activity2.java
@@ -6,6 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Soft1714080902331Activity2 extends AppCompatActivity {
 
--- students/soft1714080902331/Soft1714080902331Activity3.java
+++ students/soft1714080902331/Soft1714080902331Activity3.java
@@ -1,11 +1,7 @@
 package edu.hzuapps.androidlabs.soft1714080902331;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class Soft1714080902331Activity3 extends AppCompatActivity {
 
