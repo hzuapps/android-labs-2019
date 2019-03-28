2c4532ec37eecbb60b32f3bb97aaeb1f1ba622ac
Wuhaochao <582830851@qq.com>
2019-03-28 22:56:01 +0800

    实验四

---
+++ students/soft1714080902111/实验4/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902111/Soft1714080902111TestActivity31.java
@@ -0,0 +1,12 @@
+package edu.hzuapps.androidlabs.soft1714080902111;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Soft1714080902111TestActivity31 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft1714080902111textactivity31);
+    }
+}
