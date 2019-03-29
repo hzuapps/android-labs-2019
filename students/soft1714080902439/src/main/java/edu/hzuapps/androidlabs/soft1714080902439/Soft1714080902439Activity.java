ce431da82956fd1f2a1aa6ce9f80f5a629aca0b9
1714080902439 <741131502@qq.com>
2019-03-30 01:47:01 +0800

    生日管家实验四代码

--- students/soft1714080902439/src/main/java/edu/hzuapps/androidlabs/soft1714080902439/Soft1714080902439Activity.java
+++ students/soft1714080902439/src/main/java/edu/hzuapps/androidlabs/soft1714080902439/Soft1714080902439Activity.java
@@ -1,24 +1,48 @@
 package edu.hzuapps.androidlabs.soft1714080902439;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.soft171408092439.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class Soft1714080902439Activity extends AppCompatActivity {
 
-    private Button button;
+    private ImageButton button;
+    private EditText time;
+    private TextView textView;
+    //获取时间
+    SimpleDateFormat formatter=new SimpleDateFormat("yyyy年MM月dd日");
+    Date currentDate=new Date(System.currentTimeMillis());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1714080902439_activity);
-        button=(Button) findViewById(R.id.button);
-        //按钮绑定接口
+        button=(ImageButton) findViewById(R.id.add_button);
+        time=(EditText) findViewById(R.id.time);
+        textView=(TextView) findViewById(R.id.text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast button=Toast.makeText(edu.hzuapps.androidlabs.soft1714080902439.Soft1714080902439Activity.this,"",Toast.LENGTH_SHORT);
+                Intent intent=new Intent(edu.hzuapps.androidlabs.soft1714080902439.Soft1714080902439Activity.this,edu.hzuapps.androidlabs.soft1714080902439.Soft1714080902439AddBirthdayActivity.class);
+                startActivity(intent);
+                button.show();
+            }
+        });
     }
 
     /*//添加生日
