70ad83883afbf2dc56393518261d63ba74a50d07
jieers <774685879@qq.com>
2019-03-29 19:44:35 +0800

    第四次实验

--- students/soft1714080902204/Soft1714080902204Activity_Second.java
+++ students/soft1714080902204/Soft1714080902204Activity_Second.java
@@ -1,7 +1,10 @@
 package edu.hzuapps.androidlabs.soft1714080902204;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Soft1714080902204Activity_Second extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class Soft1714080902204Activity_Second extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1714080902204_second);
+
+
+        Button btnOpen = findViewById(R.id.button4);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Soft1714080902204Activity_Second.this,Soft1714080902204Activity_third.class));
+
+            }
+        });
+
+        btnOpen = findViewById(R.id.button5);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Soft1714080902204Activity_Second.this,Soft1714080902204Activity_third.class));
+
+            }
+        });
+
+
     }
 }
