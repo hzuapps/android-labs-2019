75073e06d88f0c8e26dfc7cb56b0d723e21708bf
nicole-rain <1025029792@qq.com>
2019-03-29 23:40:49 +0800

    #4 第四次实验

--- students/soft1714080902109/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902109/layout.java
+++ students/soft1714080902109/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902109/layout.java
@@ -1,30 +1,27 @@
 package edu.hzuapps.androidlabs.soft1714080902109;
 
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import edu.hzuapps.androidlabs.Soft1714080902109.R;
 
-public class Soft1714080902109Activity<id> extends AppCompatActivity {
+public class layout extends AppCompatActivity {
 
     private Button button;
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.soft_1714080902109_activity);
-        button = (Button) findViewById(R.id.button_1);
+        setContentView(R.layout.activity_layout);
+        button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Soft1714080902109Activity.this, addActivity.class);
+                Intent intent = new Intent(layout.this, inquiryActivity.class);
                 startActivity(intent);
             }
         });
-}}
+    }
+}
