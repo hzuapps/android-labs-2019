0e4a6fe966df29a67b3cb39228fecdaf18d3dad6
Coke1e <z13542774735@163.com>
2019-03-29 13:14:08 +0800

    #4 提交实验四代码

--- students/Soft1714080902403/app/src/main/java/edu/androidlabs/soft1714080902403/MainActivity.java
+++ students/Soft1714080902403/app/src/main/java/edu/androidlabs/soft1714080902403/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,14 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView =(TextView)findViewById(R.id.coke);
-
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, Soft1714080902403.class);
-                startActivity(intent);
-            }
-        });
     }
+    public void click(View view){
+        Intent intent=new Intent(this,Soft1714080902403.class);
+        startActivity(intent);
+    }
+
 }
+
+
