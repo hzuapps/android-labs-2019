89f4b0d92e09657e7723ced719a3790e81f4cb25
kaluozi <784745401@qq.com>
2019-03-29 13:05:19 +0800

    第三次实验

--- students/soft1714080902401/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902401/Soft1714080902401Activity.java
+++ students/soft1714080902401/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902401/Soft1714080902401Activity.java
@@ -1,13 +1,29 @@
 package edu.hzuapps.androidlabs.soft1714080902401;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Soft1714080902401Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.soft_1714080902401_activity);
-    }
+        setContentView(R.layout.activity_soft_1714080902401);
+
+    Button begin = (Button) findViewById(R.id.Begin);
+        begin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Soft1714080902401Activity.this,Soft1714080902401_01Activity.class);
+            startActivity(intent);
+        }
+    });
 }
+
+}
+
