a81d6ca8f0debfae9d5fbd97c18635a3dec3aef1
csl275 <1564399427@qq.com>
2019-03-29 21:49:56 +0800

    Revert "Revert "# 实验三""
    This reverts commit b881f85289633ef68752d589af2e143fa83a4f56.

---
+++ students/Com1714080901139/MainActivity.java
@@ -0,0 +1,31 @@
+package edu.hzuapps.androidlabs.com1714080901139;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=(TextView)findViewById(R.id.btn1);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Com1714080901139.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
