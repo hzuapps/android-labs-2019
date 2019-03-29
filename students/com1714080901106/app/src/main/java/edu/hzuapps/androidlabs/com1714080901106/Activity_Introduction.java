dd9026643784cc04302229dc3781096bac790b48
XieZehuai <1544498268@qq.com>
2019-03-29 20:57:29 +0800

    提交实验四代码

---
+++ students/com1714080901106/app/src/main/java/edu/hzuapps/androidlabs/com1714080901106/Activity_Introduction.java
@@ -0,0 +1,23 @@
+package edu.hzuapps.androidlabs.com1714080901106;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+public class Activity_Introduction extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_introduction, container, false);
+    }
+}
