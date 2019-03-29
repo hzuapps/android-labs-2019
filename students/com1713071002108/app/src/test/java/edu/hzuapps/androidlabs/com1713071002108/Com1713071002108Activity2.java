9cf66317621ca9a33bd6cae12ed5fda71e4fd6cc
Swag7zai <1943493146@qq.com>
2019-03-29 21:38:58 +0800

    # 实验四代码

---
+++ students/com1713071002108/app/src/test/java/edu/hzuapps/androidlabs/com1713071002108/Com1713071002108Activity2.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.androidlabs.com1713071002108;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class Com1713071002108Activity2 extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.com_1713071002108_activity2);
+    }
+}
