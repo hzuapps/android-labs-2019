f2bf44d37d6e0bca393252d642004830f50b28fa
csl275 <1564399427@qq.com>
2019-03-29 21:56:45 +0800

    #实验四
    这是实验四

---
+++ students/Com1714080901139/实验四/Com1714080901139.java
@@ -0,0 +1,15 @@
+
+package edu.hzuapps.androidlabs.com1714080901139;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Com1714080901139 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+               super.onCreate(savedInstanceState);
+        setContentView(R.layout.com_1714080901139_activity);
+    }
+}
+//
