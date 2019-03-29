fa544284a6c9eba8fe0d70bc4a7d2b6250efc22a
lbw7 <970967619@qq.com>
2019-03-29 22:03:43 +0800

    #4 #lbw7 第四次实验

--- students/soft1714080902329/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902329/Activity2.java
+++ students/soft1714080902329/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902329/Activity2.java
@@ -1,15 +1,13 @@
 package edu.hzuapps.androidlabs.soft1714080902329;
 
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Brother extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_brother);
+        setContentView(R.layout.activity_2);
     }
-
 }
