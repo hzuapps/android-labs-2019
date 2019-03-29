51d4e03581cb296e56e7909ffe6431a4f7aa7f48
谢志锋 <48149905+anceker@users.noreply.github.com>
2019-03-29 20:02:41 +0800

    #3 #anceker 第3次实验

---
+++ students/实验03/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902313/Soft_1714080902313_Activity3.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.soft1714080902313;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1714080902313Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902313_activity3);
+    }
+}
