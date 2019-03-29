7f2eb74a30d52f37bf96aab6a12f9a4afb2243a5
LLSSzuramaru <374868511@qq.com>
2019-03-29 20:01:40 +0800

    #4 #256 第四次实验

---
+++ students/soft1714080902112/Miku2/app/src/main/java/com/example/miku/V1_Activity.java
@@ -0,0 +1,11 @@
+package com.example.miku;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class V1_Activity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.v1);
+    }
+}
