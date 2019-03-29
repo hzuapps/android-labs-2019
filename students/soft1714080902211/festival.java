1c5887259a12c481841b5200305554b617a6277b
Tohsakaemmmmm <1036581917@qq.com>
2019-03-29 20:00:05 +0800

    第四次实验

---
+++ students/soft1714080902211/festival.java
@@ -0,0 +1,18 @@
+package edu.hzuapps.androidlabs.soft1714080902211;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class festival extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_festival);
+    }
+
+}
