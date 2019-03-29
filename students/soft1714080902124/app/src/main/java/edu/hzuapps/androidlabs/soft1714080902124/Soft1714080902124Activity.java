c57b48136a817b2a770b28b004f70fdadcd05f1d
FFBOND <1046422249@qq.com>
2019-03-30 00:46:01 +0800

    第四次实验

--- students/soft1714080902124/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902124/Soft1714080902124Activity.java
+++ students/soft1714080902124/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902124/Soft1714080902124Activity.java
@@ -1,26 +1,28 @@
 package edu.hzuapps.androidlabs.soft1714080902124;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.example.myfirstapp.R;
 
 public class Soft1714080902124Activity extends AppCompatActivity{
 
-    private EditText editText;
 
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1714080902124_activity);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
 
         button = (Button) findViewById(R.id.button);
-//        editText =(EditTextActivity) findViewById(R.id.edit_text);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
