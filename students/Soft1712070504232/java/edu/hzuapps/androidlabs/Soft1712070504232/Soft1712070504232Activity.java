ea17d62660146dda05188f5d1e83522063705539
Hzu-sick <1312203573@qq.com>
2019-03-29 18:37:17 +0800

    实验4

--- students/Soft1712070504232/java/edu/hzuapps/androidlabs/Soft1712070504232/Soft1712070504232Activity.java
+++ students/Soft1712070504232/java/edu/hzuapps/androidlabs/Soft1712070504232/Soft1712070504232Activity.java
@@ -4,16 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Soft1712070504232Activity extends Activity {
-    private TextView textView;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1712070504232_activity);
-        textView = (TextView) findViewById(R.id.phone);
-        textView.setOnClickListener(new View.OnClickListener() {
+        button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Soft1712070504232Activity.this, Soft1712070504232Activity2.class);
