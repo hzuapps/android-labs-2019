265fa2d7d1d68192f3b8b08178381b8b1a5bd052
Tohsakaemmmmm <1036581917@qq.com>
2019-03-31 00:56:43 +0800

    第三四次实验

--- students/soft1714080902211/java/edu/hzuapps/androidlabs/soft1714080902211/festival.java
+++ students/soft1714080902211/java/edu/hzuapps/androidlabs/soft1714080902211/festival.java
@@ -12,7 +12,7 @@ public class Festival extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_festival);
+        setContentView(R.layout.activity_festival2);
     }
 
 }
