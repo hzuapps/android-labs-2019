2ea1c1fb31599bb8973570c26d4cb40c173939ac
Tohsakaemmmmm <1036581917@qq.com>
2019-03-30 21:13:36 +0800

    第三四次实验

--- students/soft1714080902211/java/edu/hzuapps/androidlabs/soft1714080902211/festival.java
+++ students/soft1714080902211/java/edu/hzuapps/androidlabs/soft1714080902211/festival.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class festival extends AppCompatActivity {
+public class Festival extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,3 +16,4 @@ public class festival extends AppCompatActivity {
     }
 
 }
+
