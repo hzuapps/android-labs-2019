ce1bb9c18fcbe6b6c98606a1438425e3b6ccec91
unknown <you@example.com>
2019-03-30 15:38:43 +0800

    22

---
+++ students/soft1709081602232/java/edu/hzuapps/androidlabs/soft1709081602232/MainActivity.java
@@ -0,0 +1,25 @@
+package soft1709081602232.androidlabs.hzuapps.edu.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
