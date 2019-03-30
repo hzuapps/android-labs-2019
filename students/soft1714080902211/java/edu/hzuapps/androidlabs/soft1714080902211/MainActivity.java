c385068f4d7ef23eaeaefe5afe9e8cd0779e3bb7
Tohsakaemmmmm <1036581917@qq.com>
2019-03-30 09:39:50 +0800

    第三四次实验

---
+++ students/soft1714080902211/java/edu/hzuapps/androidlabs/soft1714080902211/MainActivity.java
@@ -0,0 +1,32 @@
+package edu.hzuapps.androidlabs.soft1714080902211;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Soft1714080902211festival.class);
+                startActivity(intent);
+            }
+        });
+        Button add = (Button) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this, festival.class);
+                startActivity(intent2);
+            }
+        });
+    }
+}
