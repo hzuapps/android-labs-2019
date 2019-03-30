51bb4680c2159a50a74549413f1858167335d1bd
caizelin111 <2559656547@qq.com>
2019-03-30 17:50:56 +0800

    #2 第二次提交

---
+++ students/soft1714080902342/shiyan2/Soft1714080902342Activity.java
@@ -0,0 +1,13 @@
+package com.example.asus.soft1714080902342activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1714080902342Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902342);
+    }
+}
