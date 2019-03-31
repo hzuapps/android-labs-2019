04ffd52a83d234678657e037a863bfd0715a860a
cjfgg <1043489873@qq.com>
2019-04-01 00:41:06 +0800

    #4 第四次实验

---
+++ students/soft1714080902319/Soft1714080902319/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902319/Soft1714080902319_2.java
@@ -0,0 +1,20 @@
+package edu.hzuapps.androidlabs.soft1714080902319;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+
+public class Soft1714080902319_2 extends AppCompatActivity {
+
+    private TextView textview01;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft1714080902319_2);
+    textview01=(TextView) findViewById(R.id.textview01);
+
+    }
+}
+
