45ae47d9a3517cbd85c3ad4f7cfcd015cd2c77e9
cjfgg <1043489873@qq.com>
2019-04-01 02:10:54 +0800

    #4 第4次实验

---
+++ students/soft1714080902319/Soft1714080902319/app/src/main/java/edu/hzuapps/androidlabs/shiyan4/app/src/main/java/edu/hzuapps/androidlabs/shiyan4/soft1714080902319_2.java
@@ -0,0 +1,16 @@
+package edu.hzuapps.androidlabs.shiyan4;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class soft1714080902319_2 extends AppCompatActivity {
+    private TextView textview1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft1714080902319_2);
+        textview1=(TextView) findViewById(R.id.textview1);
+
+    }
+}
