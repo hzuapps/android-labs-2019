45b4563b60119c07a089fa7d4bd5b1ca79590284
Android-lgw <934424456@qq.com>
2019-04-01 21:03:52 +0800

    #3 #279 提交实验三代码文件

---
+++ students/soft1714080902325/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902325/HomeActivity.java
@@ -0,0 +1,16 @@
+package edu.hzuapps.androidlabs.soft1714080902325;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
+
+
