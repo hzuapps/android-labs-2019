e25f632508924ff3d9bc9140a66515885e5ee83a
= <=>
2019-04-03 01:04:13 +0800

    #4 #942 第四次实验

---
+++ students/Soft1714080902335/shiyan4/Soft1714080902335Activity.java
@@ -0,0 +1,37 @@
+package edu.hzuapps.androidlabs.soft1714080902335activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Soft1714080902335Activity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902335);
+        button=(Button)findViewById(R.id.di01);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Soft1714080902335Activity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button=(Button)findViewById(R.id.di02);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Soft1714080902335Activity.this,ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
