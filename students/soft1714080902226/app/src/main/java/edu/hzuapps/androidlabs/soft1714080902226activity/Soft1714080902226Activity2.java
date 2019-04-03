c926db79a70a73729c800a196807e334b93cdafb
unknown <949602204@qq.com>
2019-04-03 20:00:15 +0800

    #5 #muxuny第五次实验

--- students/soft1714080902226/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902226activity/Soft1714080902226Activity2.java
+++ students/soft1714080902226/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902226activity/Soft1714080902226Activity2.java
@@ -4,7 +4,14 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 import edu.hzuapps.androidlabs.soft1714080902226activity.view.SlideMenu;
 
 public class Soft1714080902226Activity2 extends AppCompatActivity {
@@ -13,6 +20,10 @@ public class Soft1714080902226Activity2 extends AppCompatActivity {
 
     private ImageView btn_back;
     private SlideMenu slideMenu;
+
+    private EditText et_info;
+    private Button btn_read;
+    private  Button  btn_save;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,5 +40,49 @@ public class Soft1714080902226Activity2 extends AppCompatActivity {
                 slideMenu.switchMenu();
             }
         });
+
+        et_info=(EditText)findViewById(R.id.et_info);
+        btn_read=(Button)findViewById(R.id.btn_read);
+        btn_save=(Button)findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new ButtonListener());
+        btn_read.setOnClickListener(new ButtonListener());
     }
+
+    private class ButtonListener implements View.OnClickListener{
+
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_save:
+                    String saveInfo=et_info.getText().toString().trim();
+                    FileOutputStream fos;
+                    try {
+                        fos=openFileOutput("data.txt",MODE_APPEND);//把文件输出到data中
+                        fos.write(saveInfo.getBytes());//将我们写入的字符串变成字符数组）
+                        fos.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Soft1714080902226Activity2.this,"数据保存成功",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btn_read:
+                    String content="";
+                    try {
+                        FileInputStream fis=openFileInput("data.txt");
+                        byte [] buffer=new  byte[fis.available()];
+                        fis.read(buffer);
+                        content=new String(buffer);
+                        fis.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Soft1714080902226Activity2.this,"保存的数据是"+content,Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+            }
+        }
+    }
+
+
 }
