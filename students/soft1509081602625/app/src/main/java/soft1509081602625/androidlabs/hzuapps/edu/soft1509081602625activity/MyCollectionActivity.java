db41b6c1c09e088dce096ca6e2ec9a7a84a7ef49
SunJiashun <316218906@qq.com>
2019-04-04 20:51:48 +0800

    #5 SunJiashun 实验五

---
+++ students/soft1509081602625/app/src/main/java/soft1509081602625/androidlabs/hzuapps/edu/soft1509081602625activity/MyCollectionActivity.java
@@ -0,0 +1,23 @@
+package soft1714080902407.androidlabs.hzuapps.edu.soft1509081602625activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.Buffer;
+
+public class MyCollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_collection_activity);
+
+    }
+}
