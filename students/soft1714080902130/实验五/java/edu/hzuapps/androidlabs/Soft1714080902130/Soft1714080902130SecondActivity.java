6f2adfea330e491a132647a1f781dfd8e99f26db
zhuang <3117405160@qq.com>
2019-04-05 10:07:52 +0800

    第五次实验

---
+++ students/soft1714080902130/实验五/java/edu/hzuapps/androidlabs/Soft1714080902130/Soft1714080902130SecondActivity.java
@@ -0,0 +1,64 @@
+package edu.hzuapps.androidlabs.Soft1714080902130;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Soft1714080902130SecondActivity extends AppCompatActivity {
+    public static final String SaveFileName="file_activities.txt";
+    private EditText editText;
+    private Button save_button;
+    private Button load_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902130_second);
+        editText=(EditText)findViewById(R.id.et_info);
+        save_button=(Button)findViewById(R.id.save_button);
+        load_button=(Button)findViewById(R.id.load_button);
+        save_button.setOnClickListener(new ButtonListener());
+        load_button.setOnClickListener(new ButtonListener());
+    }
+    private class ButtonListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.save_button:
+                    String saveInfo=editText.getText().toString().trim();
+                    FileOutputStream fos;
+                    try {
+                        fos=openFileOutput(SaveFileName,MODE_APPEND);
+                        fos.write(saveInfo.getBytes());
+                        fos.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Soft1714080902130SecondActivity.this,"数据保存成功",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.load_button:
+                    String content="";
+                    try {
+                        FileInputStream fis=openFileInput(SaveFileName);
+                        byte [] buffer=new  byte[fis.available()];
+                        fis.read(buffer);
+                        content=new String(buffer);
+                        fis.close();
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Soft1714080902130SecondActivity.this,"保存活动为："+content,Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+            }
+        }
+    }
+
+}
+
