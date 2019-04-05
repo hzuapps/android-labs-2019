ed2417c7c075626aac3472a6c1aea668eba76baf
1714080902116 <3399240862@qq.com>
2019-04-05 23:40:42 +0800

    第五次实验

--- students/soft1714080902116/MyApplicationfour/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902116/Main2ActivitySoft1714080902116.java
+++ students/soft1714080902116/MyApplicationfour/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902116/Main2ActivitySoft1714080902116.java
@@ -1,18 +1,37 @@
 package edu.hzuapps.androidlabs.soft1714080902116;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class Main2ActivitySoft1714080902116 extends AppCompatActivity {
 
+    private EditText et_info;
+    private Button btn_save;
+    private Button btn_read;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2_activity_soft1714080902116);
-        Button button2 = (Button) findViewById(R.id.button_2);
+
+        et_info = (EditText) findViewById(R.id.et_info);
+        btn_save = (Button) findViewById(R.id.btn_save);
+        btn_read = (Button) findViewById(R.id.btn_read);
+        button2=(Button) findViewById(R.id.button_2);
+        btn_save.setOnClickListener(new ButtonListener());
+        btn_read.setOnClickListener(new ButtonListener());
+        button2.setOnClickListener(new ButtonListener());
+
+       /* Button button2 = (Button) findViewById(R.id.button_2);
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -21,6 +40,53 @@ public class Main2ActivitySoft1714080902116 extends AppCompatActivity {
                 startActivity(intent);
             }
 
-        });
+        });*/
     }
+
+    private class ButtonListener implements View.OnClickListener{
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_save:
+
+                    //存储
+                    String sendinfo = et_info.getText().toString().trim();
+                    FileOutputStream fos;
+                    try {
+                        //保存人物
+                        fos = openFileOutput("data.txt", Context.MODE_APPEND);
+                        fos.write(sendinfo.getBytes());
+                        fos.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Main2ActivitySoft1714080902116.this, "人物保存成功", Toast.LENGTH_SHORT).show();
+                    break;
+
+                //获取
+                case R.id.btn_read:
+                    String content = "";
+                    try {
+                        FileInputStream fis = openFileInput("data.txt");
+                        byte[] buffer = new byte[fis.available()];
+                        fis.read(buffer);
+                        content = new String(buffer);
+                        fis.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Main2ActivitySoft1714080902116.this, "保存的数据是：" + content, Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.button_2:
+                    Intent intent = new Intent( Main2ActivitySoft1714080902116.this,MainActivitySoft1714080902116.class);
+                    startActivity(intent);
+
+
+                default:
+                    break;
+
+            }
+        }
+    }
+
+
 }
--- students/soft1714080902116/MyApplicationfour/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902116/MainActivitySoft1714080902116.java
+++ students/soft1714080902116/MyApplicationfour/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902116/MainActivitySoft1714080902116.java
@@ -1,11 +1,16 @@
 package edu.hzuapps.androidlabs.soft1714080902116;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class MainActivitySoft1714080902116 extends AppCompatActivity {
 
@@ -14,6 +19,7 @@ public class MainActivitySoft1714080902116 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -25,4 +31,7 @@ public class MainActivitySoft1714080902116 extends AppCompatActivity {
 
         });
     }
+
+
+
 }
