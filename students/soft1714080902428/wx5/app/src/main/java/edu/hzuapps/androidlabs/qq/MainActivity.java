a2fc06cd475623d018937cab8178121ff176f1aa
skrhhh <735596553@qq.com>
2019-04-05 20:25:28 +0800

    #5 #677 第5次实验

---
+++ students/soft1714080902428/wx5/app/src/main/java/edu/hzuapps/androidlabs/qq/MainActivity.java
@@ -0,0 +1,50 @@
+package edu.hzuapps.androidlabs.qq;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ImageView imageView =(ImageView) findViewById(R.id.imageView3);
+        imageView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+        Button button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(MainActivity.this,Main4Activity.class);
+                startActivity(intent);
+            }
+        });
+        Button button2=(Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
