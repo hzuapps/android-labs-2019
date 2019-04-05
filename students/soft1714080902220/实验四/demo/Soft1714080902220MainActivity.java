fc288ef0808eb0c6eef60034b71c7ce8456abd77
Chenhuanpen <1245532581@qq.com>
2019-04-05 17:09:49 +0800

    #4 第四次实验

---
+++ students/soft1714080902220/实验四/demo/Soft1714080902220MainActivity.java
@@ -0,0 +1,24 @@
+package edu.hzuapps.androidlabs.demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Soft1714080902220MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Soft1714080902220MainActivity.this, Soft1714080902220DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
