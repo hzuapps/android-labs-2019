168efd0b59a81bc9a6893a99dd7b32fd849c0eab
LLSSzuramaru <374868511@qq.com>
2019-04-05 18:31:06 +0800

    #248 第五次实验

---
+++ students/soft1714080902112/SportsExchangeCommunity/app/src/main/java/com/example/sportsexchangecommunity/JueActivity.java
@@ -0,0 +1,11 @@
+package com.example.sportsexchangecommunity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class JueActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jue);
+    }
+}
