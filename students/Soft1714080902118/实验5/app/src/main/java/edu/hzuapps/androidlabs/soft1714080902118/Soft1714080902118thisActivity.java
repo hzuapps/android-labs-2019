e43051cdbf30aff37dc9c29a907002e27bd4815d
yuehuawei <976680118@qq.com>
2019-04-05 15:28:55 +0800

    #5 第五次实验

---
+++ students/Soft1714080902118/实验5/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902118/Soft1714080902118thisActivity.java
@@ -0,0 +1,82 @@
+package edu.hzuapps.androidlabs.soft1714080902118;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Soft1714080902118thisActivity extends AppCompatActivity {
+        private EditText edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902118_thisactivity);
+        edit =(EditText) findViewById(R.id.edit);
+        String inputText=load();
+        if(!TextUtils.isEmpty(inputText)){
+            edit.setText(inputText);
+            edit.setSelection(inputText.length());
+            Toast.makeText(this,"读取成功"+"上次的数据是："+inputText, Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        String inputText=edit.getText().toString();
+        save(inputText);
+    }
+    public void save(String inputText){
+        FileOutputStream out=null;
+        BufferedWriter writer=null;
+        try{
+            out=openFileOutput("data",MODE_PRIVATE);
+            writer=new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if (writer!=null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public String load(){
+        FileInputStream in=null;
+        BufferedReader reader=null;
+        StringBuilder content =new StringBuilder();
+        try{
+            in=openFileInput("data");
+            reader=new BufferedReader(new InputStreamReader(in));
+            String line="";
+            while ((line=reader.readLine())!=null){
+                content.append(line);
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            if(reader!=null){
+                try{
+                    reader.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return  content.toString();
+    }
+}
