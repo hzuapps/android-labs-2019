991a4845b7c77962eaa4e6e6d458b6583db515cf
luxiaoshanhhh <2682786040@qq.com>
2019-04-05 12:27:32 +0800

    提交实验五代码

---
+++ students/soft1709081602603/RunCount/app/src/main/java/edu/hzuapps/androidlabs/soft1709081602603/Soft1709081602603SetActivity.java
@@ -0,0 +1,62 @@
+package edu.hzuapps.androidlabs.soft1709081602603;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Soft1709081602603SetActivity extends AppCompatActivity {
+    private EditText et_info,et_info1,et_info2;
+    private Button btn_read;
+    private  Button  btn_save;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1709081602603_set);
+        et_info=(EditText)findViewById(R.id.et_info);
+        et_info1=(EditText)findViewById(R.id.et_info1);
+        et_info2=(EditText)findViewById(R.id.et_info2);
+        btn_read=(Button)findViewById(R.id.btn_read);
+        btn_save=(Button)findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new ButtonListener());
+        btn_read.setOnClickListener(new ButtonListener());
+    }
+    private class ButtonListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_save:
+                    String saveInfo=et_info.getText().toString().trim()+","+et_info1.getText().toString().trim()+","+et_info2.getText().toString().trim();
+                    FileOutputStream fos;
+                    try {
+                        fos=openFileOutput("data.txt",MODE_APPEND);
+                        fos.write(saveInfo.getBytes());
+                        fos.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Soft1709081602603SetActivity.this,"提交成功",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btn_read:
+                    String content="";
+                    try {
+                        FileInputStream fis=openFileInput("data.txt");
+                        byte [] buffer=new  byte[fis.available()];
+                        fis.read(buffer);
+                        content=new String(buffer);
+                        fis.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Soft1709081602603SetActivity.this,"提交的内容是"+content,Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+            }
+        }
+    }
+}
