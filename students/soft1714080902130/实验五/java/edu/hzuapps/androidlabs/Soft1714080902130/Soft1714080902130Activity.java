c4e702c29a3c74fe68ad6bdf63e2545074fc826b
zhuang <3117405160@qq.com>
2019-04-06 00:08:00 +0800

    message

---
+++ students/soft1714080902130/实验五/java/edu/hzuapps/androidlabs/Soft1714080902130/Soft1714080902130Activity.java
@@ -0,0 +1,26 @@
+package edu.hzuapps.androidlabs.soft1714080902130;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Soft1714080902130Activity extends AppCompatActivity {
+    private ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902130);
+        img = findViewById(R.id.img1);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Soft1714080902130Activity.this,
+                        Soft1714080902130LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
