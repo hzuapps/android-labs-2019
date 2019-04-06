96e6c623488b40929ae73e86a168bf80eb36758b
xuxiaoting1128 <524269667@qq.com>
2019-04-06 16:43:53 +0800

    5实验五代码

--- students/com1712070504104/app/src/main/java/edu/hzuapps/androidlabs/com1712070504104/Com1712070504104Activity.java
+++ students/com1712070504104/app/src/main/java/edu/hzuapps/androidlabs/com1712070504104/Com1712070504104Activity.java
@@ -1,29 +1,61 @@
 package edu.hzuapps.androidlabs.com1712070504104;
-
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
-
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class Com1712070504104Activity extends AppCompatActivity {
-
+private EditText et_info;
+private Button btn_save;
+private Button btn_read;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.com_1712070504104_activity);
-        TextView text=findViewById(R.id.app_name);
-        text.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Com1712070504104Activity.this,Com1712070504104Activity2.class);
-                startActivity(intent);
+        et_info = (EditText) findViewById(R.id.et_info);
+        btn_read = (Button) findViewById(R.id.btn_read);
+        btn_save = (Button) findViewById(R.id.btn_save);
+        btn_read.setOnClickListener(new ButtonListener());
+        btn_save.setOnClickListener(new ButtonListener());
+    }
+       private class ButtonListener implements View.OnClickListener {
+        public void onClick(View v){
+            switch (v.getId()){
+                case R.id.btn_save:
+                    String saveinfo=et_info.getText().toString().trim();
+                    FileOutputStream fos;
+                    try{
+                        fos=openFileOutput("data.txt", Context.MODE_APPEND);
+                        fos.write(saveinfo.getBytes());
+                        fos.close();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Com1712070504104Activity.this,"生日信息保存成功",0).show();
+                    break;
+                case R.id.btn_read:
+                    String content="";
+                    try{
+                        FileInputStream fis=openFileInput("data.txt");
+                        byte[] buffer=new byte[fis.available()];
+                        fis.read(buffer);
+                        content=new String(buffer);
+                        fis.close();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Com1712070504104Activity.this,"保存的生日信息是："+content,0).show();
+                    break;
+                    default:
+                        break;
+
             }
-        });
+        }
+       }
 
     }
-
-}
