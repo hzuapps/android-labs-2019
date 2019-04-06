3e94b5d87207af66a713b7914e53fefa677f46a1
cqr22 <1309382951@qq.com>
2019-04-06 19:47:07 +0800

    #5 #313 第五次实验

--- students/soft1706081301317/app/src/main/java/com/example/soft1706081301317sweepactivity/SweepActivity.java
+++ students/soft1706081301317/app/src/main/java/com/example/soft1706081301317sweepactivity/SweepActivity.java
@@ -1,5 +1,6 @@
 package com.example.soft1706081301317sweepacticity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,13 +8,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SweepActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sweep_acticity);
-
+        Button button = (Button)findViewById(R.id.save);
+        final EditText editText = findViewById(R.id.username);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("username",editText.getText().toString());
+                editor.apply();
+            }
+        });
     }
 
 }
