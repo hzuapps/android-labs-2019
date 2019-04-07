db1f5f965d6c39083c283791e81fdac3c9d3a018
1714080902141 <1321380131@qq.com>
2019-04-07 14:21:26 +0800

    #1059实验五

--- students/soft1714080902141/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902141/Main2Activity.java
+++ students/soft1714080902141/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902141/Main2Activity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
@@ -13,45 +12,109 @@ import java.io.FileOutputStream;
 public class Main2Activity extends AppCompatActivity {
 
 
-    public Button button;
-    public EditText edit_text;
+    public Button button1;
+    public Button button2;
+    public Button button3;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
-       button=findViewById(R.id.button3);
-              edit_text=findViewById(R.id.a3);
+            button1=findViewById(R.id.button1);
+            button2=findViewById(R.id.button2);
+            button3=findViewById(R.id.button3);
 
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
+//
+            button1.setOnClickListener(new View.OnClickListener() {
+               @Override
                            public void onClick(View v) {
-                     String saveinfo = edit_text.getText().toString().trim();
+                    String saveinfo = "菜鸟";
                      FileOutputStream fos;
-                     FileInputStream fis;
+                    FileInputStream fis;
                      String content = "";
 
                      try {
                          fos = openFileOutput("data.txt", MODE_PRIVATE);
                          fos.write(saveinfo.getBytes());
                          fos.close();
-                     } catch (Exception e) {
-                         e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
                      }
                      try {
 
                           fis = openFileInput("data.txt");
-                         byte[] buffer = new byte[fis.available()];
-                         fis.read(buffer);
+                        byte[] buffer = new byte[fis.available()];
+                        fis.read(buffer);
 
                          content = new String(buffer);
                          fis.close();
 
-                     } catch (Exception e) {
-                         e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
                      }
-                     Toast.makeText(Main2Activity.this, "欢迎您，" + content + "!", Toast.LENGTH_LONG).show();
+                     Toast.makeText(Main2Activity.this, "保存成功，游戏难度为：" + content , Toast.LENGTH_LONG).show();
+                }
+            });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String saveinfo = "菜鸡";
+                FileOutputStream fos;
+                FileInputStream fis;
+                String content = "";
+
+                try {
+                    fos = openFileOutput("data.txt", MODE_PRIVATE);
+                    fos.write(saveinfo.getBytes());
+                    fos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                try {
+
+                    fis = openFileInput("data.txt");
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+
+                    content = new String(buffer);
+                    fis.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
 
-            });
+                Toast.makeText(Main2Activity.this, "保存成功，游戏难度为：" + content , Toast.LENGTH_LONG).show();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String saveinfo = "菜鸭";
+                FileOutputStream fos;
+                FileInputStream fis;
+                String content = "";
+
+                try {
+                    fos = openFileOutput("data.txt", MODE_PRIVATE);
+                    fos.write(saveinfo.getBytes());
+                    fos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                try {
+
+                    fis = openFileInput("data.txt");
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+
+                    content = new String(buffer);
+                    fis.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                Toast.makeText(Main2Activity.this, "保存成功，游戏难度为：" + content , Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
