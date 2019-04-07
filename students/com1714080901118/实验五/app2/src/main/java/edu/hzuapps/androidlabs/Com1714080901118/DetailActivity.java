2bf167d0eb70871c45de00c146e25806d1a058a7
halolol <1569301414@qq.com>
2019-04-07 18:09:22 +0800

    #5 第五次实验

---
+++ students/com1714080901118/实验五/app2/src/main/java/edu/hzuapps/androidlabs/Com1714080901118/DetailActivity.java
@@ -0,0 +1,71 @@
+package com.example.aishop;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private EditText et_count;
+    private Button btn_save;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        et_count=(EditText)findViewById(R.id.et_count);
+        btn_save=(Button)findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new ButtonListener());
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private class ButtonListener implements View.OnClickListener {
+        @SuppressLint("WrongConstant")
+        public void onClick(View v){
+          switch(v.getId()){
+              case R.id.btn_save:
+                  String saveinfo=et_count.getText().toString().trim();
+                  FileOutputStream fos;
+                  try{
+                      fos=openFileOutput("data.txt", Context.MODE_APPEND);
+                      fos.write(saveinfo.getBytes());
+                      fos.close();
+                  }catch(Exception e){
+                      e.printStackTrace();
+                  }
+                  Toast.makeText(DetailActivity.this,"添加成功",0).show();
+                  break;
+                  default:
+                      break;
+          }
+           }
+    }
+
+}
