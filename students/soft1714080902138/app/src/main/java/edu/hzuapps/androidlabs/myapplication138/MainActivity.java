b0e8f35c63844db55697cee5a87bf2ea701cfc06
woshigaoshou <1258006989@qq.com>
2019-04-08 00:16:39 +0800

    第5次实验

--- students/soft1714080902138/app/src/main/java/edu/hzuapps/androidlabs/myapplication138/MainActivity.java
+++ students/soft1714080902138/app/src/main/java/edu/hzuapps/androidlabs/myapplication138/MainActivity.java
@@ -1,89 +1,169 @@
 package edu.hzuapps.androidlabs.myapplication138;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit = (EditText) findViewById(R.id.edit);
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)){
+            edit.setText(inputText);
+            edit.setSelection(inputText.length());
+            Toast.makeText(this,"Restoring succeeded",Toast.LENGTH_SHORT).show();
+        }
 
         Button button1 = (Button) findViewById(R.id.button_1);
-        button1.setOnClickListener(new View.OnClickListener(){
-                                        @Override
-                                        public void onClick(View v){
-                                            Intent intent = new Intent(MainActivity.this, Button1.class);
-                                            startActivity(intent);
+        button1.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           Intent intent = new Intent(MainActivity.this, Button1.class);
+                                           startActivity(intent);
 
-                                        }
-                                    }
+                                       }
+                                   }
         );
 
 
         Button button2 = (Button) findViewById(R.id.button_2);
-        button2.setOnClickListener(new View.OnClickListener(){
-                                        @Override
-                                        public void onClick(View v){
-                                            Intent intent = new Intent(MainActivity.this, Button2.class);
-                                            startActivity(intent);
+        button2.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           Intent intent = new Intent(MainActivity.this, Button2.class);
+                                           startActivity(intent);
 
-                                        }
-                                    }
+                                       }
+                                   }
         );
 
         Button button3 = (Button) findViewById(R.id.button_3);
-        button3.setOnClickListener(new View.OnClickListener(){
-                                        @Override
-                                        public void onClick(View v){
-                                            Intent intent = new Intent(MainActivity.this, Button3.class);
-                                            startActivity(intent);
+        button3.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           Intent intent = new Intent(MainActivity.this, Button3.class);
+                                           startActivity(intent);
 
-                                        }
-                                    }
+                                       }
+                                   }
         );
 
 
         Button button4 = (Button) findViewById(R.id.button_4);
-        button4.setOnClickListener(new View.OnClickListener(){
-                                        @Override
-                                        public void onClick(View v){
-                                            Intent intent = new Intent(MainActivity.this, Button4.class);
-                                            startActivity(intent);
+        button4.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           Intent intent = new Intent(MainActivity.this, Button4.class);
+                                           startActivity(intent);
 
-                                        }
-                                    }
+                                       }
+                                   }
         );
 
         Button button5 = (Button) findViewById(R.id.button_5);
-        button5.setOnClickListener(new View.OnClickListener(){
-                                        @Override
-                                        public void onClick(View v){
-                                            Intent intent = new Intent(MainActivity.this, Button5.class);
-                                            startActivity(intent);
+        button5.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           Intent intent = new Intent(MainActivity.this, Button5.class);
+                                           startActivity(intent);
 
-                                        }
-                                    }
+                                       }
+                                   }
         );
 
 
         Button button6 = (Button) findViewById(R.id.button_6);
-        button6.setOnClickListener(new View.OnClickListener(){
-                                        @Override
-                                        public void onClick(View v){
-                                            Intent intent = new Intent(MainActivity.this, Button6.class);
-                                            startActivity(intent);
+        button6.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           Intent intent = new Intent(MainActivity.this, Button6.class);
+                                           startActivity(intent);
 
-                                        }
-                                    }
+                                       }
+                                   }
         );
 
 
+
+
+
     }
+
+
+            @Override
+            protected void onDestroy() {
+                super.onDestroy();
+                String inputText = edit.getText().toString();
+                save(inputText);
+            }
+
+            public void save (String inputText) {
+                FileOutputStream out = null;
+                BufferedWriter writer = null;
+                try {
+                    out = openFileOutput("data", Context.MODE_PRIVATE);
+                    writer = new BufferedWriter(new OutputStreamWriter(out));
+                    writer.write(inputText);
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        if (writer != null) {
+                            writer.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+    public String load(){
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
+        }finally{
+            if (reader != null){
+                try{
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
+    }
+
 }
+
+
+
+
