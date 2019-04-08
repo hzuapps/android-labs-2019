148b3ca901360923ef62ad190e90651b4777db04
cxh245 <245385661@qq.com>
2019-04-08 22:16:15 +0800

    实验五代码提交 家教管理系统

--- students/com1714080901108/app/src/main/java/edu/hzuapps/androidlabs/com1714080901108/Main3Activity.java
+++ students/com1714080901108/app/src/main/java/edu/hzuapps/androidlabs/com1714080901108/Main3Activity.java
@@ -1,11 +1,13 @@
 package edu.hzuapps.androidlabs.com1714080901108;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class Main3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        Button button=(Button) findViewById(R.id.button5);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -22,6 +25,13 @@ public class Main3Activity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Main3Activity.this,SaveActivity.class);
+                startActivity(intent);
             }
         });
     }
