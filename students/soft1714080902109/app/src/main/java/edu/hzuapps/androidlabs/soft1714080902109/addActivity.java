6055a5e229e2126189d6c97c06655cc1f52ddbd3
nicole-rain <1025029792@qq.com>
2019-04-09 23:01:11 +0800

    #5 第五次实验

--- students/soft1714080902109/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902109/addActivity.java
+++ students/soft1714080902109/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902109/addActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import edu.hzuapps.androidlabs.Soft1714080902109.R;
 
