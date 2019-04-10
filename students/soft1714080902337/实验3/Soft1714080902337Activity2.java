ccff51261b2c1a4fa1f9f4cb146aca3adb8e1c39
cyx456 <2587539403@qq.com>
2019-04-10 20:04:47 +0800

    #3 实验三代码

---
+++ students/soft1714080902337/实验3/Soft1714080902337Activity2.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.soft1714080902337;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Soft1714080902337Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902337_activity2);
+    }
+}
