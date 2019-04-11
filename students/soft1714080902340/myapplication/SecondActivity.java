a03d51cc3dd663663633e63600774dfd1cadbc43
unknown <2785733582@qq.com>
2019-04-11 21:55:55 +0800

    #5 #1178 第五次实验 内部存储

---
+++ students/soft1714080902340/myapplication/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.cap.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
