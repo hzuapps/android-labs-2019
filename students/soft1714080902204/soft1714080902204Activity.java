a64d642047d9881d4c349979b6f6b7173aa2e756
jieers <774685879@qq.com>
2019-04-11 16:13:04 +0800

    实验六二次

--- students/soft1714080902204/soft1714080902204Activity.java
+++ students/soft1714080902204/soft1714080902204Activity.java
@@ -1,6 +1,7 @@
 package edu.hzuapps.androidlabs.soft1714080902204;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,5 +22,35 @@ public class Soft1714080902204Activity extends AppCompatActivity {
 
             }
         });
+        final MediaPlayer mediaPlayer=MediaPlayer.create(this,R.raw.yyz);//创建的mediaplayer对象，且装载了播放音频
+
+        Button btn_stop=findViewById(R.id.button8);
+        Button btn_pause=findViewById(R.id.button2);
+        Button btn_play = findViewById(R.id.button3);
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();//播放音频
+
+            }
+        });
+        btn_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();//暂停
+
+            }
+        });
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();//停止
+
+            }
+        });
+
+
+
+
     }
 }
