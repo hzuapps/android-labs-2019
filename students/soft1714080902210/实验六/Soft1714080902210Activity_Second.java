42d60c8fb1dd58868b268eb0f595760b2559145b
NarutoHBH <1017827570@qq.com>
2019-04-11 17:45:53 +0800

    #6 #259 第6次实验

---
+++ students/soft1714080902210/实验六/Soft1714080902210Activity_Second.java
@@ -0,0 +1,64 @@
+package edu.hzuapps.androidlabs.soft1714080902210;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Soft1714080902210Activity_Second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902210__second);
+        final MediaPlayer mediaPlayer=MediaPlayer.create(this,R.raw.christopherlarkincityoftears);
+        Button btn_play=findViewById(R.id.button4);
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+        Button btn_stop=findViewById(R.id.button11);
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+            }
+        });
+        Button btn_pause=findViewById(R.id.button12);
+        btn_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+        Button btnOpen = findViewById(R.id.button2);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Soft1714080902210Activity_Second.this,Soft1714080902210Activity_New.class));
+            }
+        });
+        Button btnOpen1 = findViewById(R.id.button3);
+        btnOpen1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Soft1714080902210Activity_Second.this,Soft1714080902210Activity_Check.class));
+            }
+        });
+        Button btnOpen2 = findViewById(R.id.button10);
+        btnOpen2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Soft1714080902210Activity_Second.this,Sort1714080902210Activity.class));
+            }
+        });
+    }
+
+}
