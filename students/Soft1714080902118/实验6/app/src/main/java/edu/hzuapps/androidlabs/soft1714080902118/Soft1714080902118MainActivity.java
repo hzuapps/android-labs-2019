13ac65731ed24db65004d0e32754b228545386b0
yuehuawei <976680118@qq.com>
2019-04-12 23:24:02 +0800

    更新实验六代码

--- students/Soft1714080902118/实验6/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902118/Soft1714080902118MainActivity.java
+++ students/Soft1714080902118/实验6/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902118/Soft1714080902118MainActivity.java
@@ -48,7 +48,7 @@ public class Soft1714080902118MainActivity extends AppCompatActivity implements 
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("http://10.0.2.2:81/get_data.json")
+                            .url("https://raw.githubusercontent.com/yuehuawei/android-labs-2019/master/students/Soft1714080902118/Json/get_data.json")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
