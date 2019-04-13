2e08de6efaa5c1a2db4b7458e9c66c2f5fa97ded
alicekagiyama <48143965+alicekagiyama@users.noreply.github.com>
2019-04-13 19:19:41 +0800

    #6 第6次实验

--- students/soft1714080902230/main/java/com/example/th/Main2Activity.java
+++ students/soft1714080902230/main/java/com/example/th/Main2Activity.java
@@ -2,6 +2,11 @@ package com.example.th;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
-}
+
+        final MyImageView myImageView = (MyImageView) findViewById(R.id.zz_view);
+        Button button_get = (Button) findViewById(R.id.button_load);
+        button_get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myImageView.setImageURL("https://raw.githubusercontent.com/alicekagiyama/png-from-pixiv/master/zz_p"+new Random().nextInt(5) +".png");
+            }
+        });
+
+
+}}
