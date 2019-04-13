96c95671d4da70e533fd650f4c329b858990f291
FFBOND <1046422249@qq.com>
2019-04-13 20:31:43 +0800

    第六次实验

---
+++ students/soft1714080902124/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902124/DownPicUtil.java
@@ -0,0 +1,97 @@
+package edu.hzuapps.androidlabs.soft1714080902124;
+
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+public class DownPicUtil {
+
+    public static void downPic(String url,DownFinishListener downFinishListener){
+        // 获取存储卡的目录
+        String filePath = Environment.getExternalStorageDirectory().getPath();
+        Log.e("路径1",filePath);
+        File file = new File(filePath+File.separator+"webViewCache");
+        Log.e("路径2",file.getPath());
+        if(!file.exists()){
+            file.mkdir();
+        }
+
+        loadPic(file.getPath(),url,downFinishListener);
+
+    }
+
+    private static void loadPic(final String filePath, final String url, final DownFinishListener downFinishListener) {
+        Log.e("下载图片的url",url);
+        new AsyncTask<Void,Void,String>(){
+            String fileName;
+            InputStream is;
+            OutputStream out;
+
+            @Override
+            protected String doInBackground(Void... voids) {
+
+                // 下载文件的名称
+                String[] split = url.split("/");
+                fileName = split[split.length - 1];
+                // 创建目标文件,不是文件夹
+                File picFile = new File(filePath + File.separator + fileName);
+                Log.e("文件名",picFile.getName());
+                if(picFile.exists()){
+                    return  picFile.getPath();
+                }
+
+                try {
+                    URL picUrl = new URL(url);
+                    //通过图片的链接打开输入流
+                    is = picUrl.openStream();
+                    if(is==null){
+                        return null;
+                    }
+                    out = new FileOutputStream(picFile);
+                    byte[] b=new byte[1024];
+                    int end ;
+                    while ((end=is.read(b))!=-1){
+                        out.write(b,0,end);
+                    }
+
+                    if(is!=null){
+                        is.close();
+                    }
+
+                    if(out!=null){
+                        out.close();
+                    }
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+
+                return picFile.getPath();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                if(s!=null){
+                    downFinishListener.getDownPath(s);
+                }
+            }
+        }.execute();
+    }
+    //下载完成回调的接口
+    public interface DownFinishListener{
+        void getDownPath(String s);
+    }
+}
--- students/soft1714080902124/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902124/Soft1714080902124Activity.java
+++ students/soft1714080902124/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902124/Soft1714080902124Activity.java
@@ -6,23 +6,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.myfirstapp.R;
 
-public class Soft1714080902124Activity extends AppCompatActivity{
+
+public class Soft1714080902124Activity extends AppCompatActivity {
 
 
     private Button button;
+    private Button button_search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1714080902124_activity);
-        ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null){
+        ActionBar actionBar = getSupportActionBar();    //标题框
+        if (actionBar != null) {
             actionBar.hide();
         }
 
-        button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);         //编辑页面
+        button_search = (Button) findViewById(R.id.search_btn); //搜索
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,7 +33,12 @@ public class Soft1714080902124Activity extends AppCompatActivity{
                 startActivity(i);
             }
         });
-
+        button_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Soft1714080902124Activity.this, SearchActivity.class);
+                startActivity(i);
+            }
+        });
     }
-
 }
