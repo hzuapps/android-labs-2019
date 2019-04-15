3718603b3b18132c0a7a90dabeefca6735c67baf
Ci-ing <1666811664@qq.com>
2019-04-16 00:32:04 +0800

    #6 #290 提交实验6代码文件

--- students/soft1714080902339/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902339/SecondActivity.java
+++ students/soft1714080902339/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902339/SecondActivity.java
@@ -1,17 +1,45 @@
 package edu.hzuapps.androidlabs.soft1714080902339;
 
-import android.content.Context;
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class SecondActivity extends AppCompatActivity {
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // 要申请的权限
+    private String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private AlertDialog dialog;
+
+    private Button button_savePhoto;
+    ImageView image;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +51,10 @@ public class SecondActivity extends AppCompatActivity {
         Button button_3 = (Button) findViewById(R.id.button_3);
         Button button_4 = (Button) findViewById(R.id.button_4);
         Button button_write = (Button) findViewById(R.id.button_write);
+        button_savePhoto = (Button) findViewById(R.id.button_savePhoto);
+        image =(ImageView) findViewById(R.id.image);
+        button_savePhoto.setOnClickListener(this);;
+        image.setOnClickListener(this);;
 
         button_0.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,8 +105,209 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
 
+        /**
+         * 设置保存印象单词图片的下载权限
+         */
 
-        //定义Button按钮的点击事件
+        // 版本判断
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // 检查该权限是否已经获取
+            int i = ContextCompat.checkSelfPermission(this, permissions[0]);
+            // 权限是否已经授权
+            if (i != PackageManager.PERMISSION_GRANTED) {
+                // 如果没有授予该权限，提示用户请求
+                showDialogTipUserRequestPermission();
+            }
+        }
+    }
+    // 提示用户该请求权限的弹出框
+    private void showDialogTipUserRequestPermission() {
+
+        new AlertDialog.Builder(this)
+                .setTitle("存储权限不可用")
+                .setMessage("应用需要获取存储空间")
+                .setPositiveButton("开启", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startRequestPermission();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).setCancelable(false).show();
+    }
+
+    // 开始提交请求权限
+    private void startRequestPermission() {
+        ActivityCompat.requestPermissions(this, permissions, 321);
+    }
+    // 用户权限 申请 的回调方法
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 321) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    // 判断用户是否 点击了不再提醒。(检测该权限是否还可以申请)
+                    boolean b = shouldShowRequestPermissionRationale(permissions[0]);
+                    if (!b) {
+                        // 提示用户进行设置界面
+                        // 手动开启权限
+                        showDialogTipUserGoToAppSettting();
+                    } else
+                        finish();
+                } else {
+                    Toast.makeText(this, "权限获取成功", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    // 提示用户去应用设置界面手动开启权限
+    private void showDialogTipUserGoToAppSettting() {
+
+        dialog = new AlertDialog.Builder(this)
+                .setTitle("存储权限不可用")
+                .setMessage("请在 设置-权限 中，允许本程序使用权限保存数据")
+                .setPositiveButton("开启", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 跳转到应用设置界面
+                        goToAppSetting();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).setCancelable(false).show();
+    }
+
+    // 跳转到当前应用的设置界面
+    private void goToAppSetting() {
+        Intent intent = new Intent();
+
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+
+        startActivityForResult(intent, 123);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123) {
+
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // 检查该权限是否已经获取
+                int i = ContextCompat.checkSelfPermission(this, permissions[0]);
+                // 权限是否已经授权
+                if (i != PackageManager.PERMISSION_GRANTED) {
+                    // 提示用户应该去应用设置界面手动开启权限
+                    showDialogTipUserGoToAppSettting();
+                } else {
+                    if (dialog != null && dialog.isShowing()) {
+                        dialog.dismiss();
+                    }
+                    Toast.makeText(this, "权限获取成功", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+     //获取网络图片
+    public Bitmap GetImageInputStream(String imageurl){
+        URL url;
+        HttpURLConnection connection=null;
+        Bitmap bitmap=null;
+        try {
+            url = new URL(imageurl);
+            connection=(HttpURLConnection)url.openConnection();
+            connection.setConnectTimeout(6000);
+            connection.setDoInput(true);
+            connection.setUseCaches(false);
+            InputStream inputStream=connection.getInputStream();
+            bitmap= BitmapFactory.decodeStream(inputStream);
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_savePhoto:
+                //在下面网络图片地址
+                new Task().execute("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3687620159,794080881&fm=26&gp=0.jpg");
+                break;
+
+            case R.id.image:
+                //点击图片后将图片保存到SD卡根目录下的Pictures文件夹内
+                SavaImage(bitmap, Environment.getExternalStorageDirectory().getPath()+"/test");
+                Toast.makeText(getBaseContext(), "印象图片保存成功", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    Handler handler=new Handler(){
+        public void handleMessage(android.os.Message msg) {
+            if(msg.what==0x123){
+                image.setImageBitmap(bitmap);
+            }
+        };
+    };
+
+
+
+    //异步线程下载图片
+
+    class Task extends AsyncTask<String, Integer, Void> {
+
+        protected Void doInBackground(String... params) {
+            bitmap=GetImageInputStream((String)params[0]);
+            return null;
+        }
+
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            Message message=new Message();
+            message.what=0x123;
+            handler.sendMessage(message);
+        }
 
     }
+     //保存位图到本地
+    /**
+     *
+     * @param bitmap
+     * @param path
+     */
+    public void SavaImage(Bitmap bitmap, String path){
+        File file=new File(path);
+        FileOutputStream fileOutputStream=null;
+        //文件夹不存在，则创建它
+        if(!file.exists()){
+            file.mkdir();
+        }
+        try {
+            fileOutputStream=new FileOutputStream(path+"/"+System.currentTimeMillis()+".png");
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100,fileOutputStream);
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
