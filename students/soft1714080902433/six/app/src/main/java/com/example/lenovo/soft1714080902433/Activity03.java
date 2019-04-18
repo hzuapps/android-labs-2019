376f1423c855a2ac0fb909f0c8db593989a433c1
lianqy0 <993960484@qq.com>
2019-04-18 22:48:46 +0800

    #6 #lianqy0 第6次实验

---
+++ students/soft1714080902433/six/app/src/main/java/com/example/lenovo/soft1714080902433/Activity03.java
@@ -0,0 +1,16 @@
+package com.example.lenovo.soft1714080902433;
+
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+@SuppressLint("Registered")
+public class Activity03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity03);
+    }
+}
