c12b5de83473f452d8ba4c2bf574d71e0d1578d0
Heywar <1197650307@qq.com>
2019-04-19 20:51:42 +0800

    #261 第五次实验

--- students/soft1714080902228/MainActivity.java
+++ students/soft1714080902228/MainActivity.java
@@ -1,35 +1,74 @@
 package edu.hzuapps.androidlabs.soft1714080902228;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v4.os.CancellationSignal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private EditText et_info;
+    private Button btn_save;
+    private Button btn_read;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //获取布局文件中的控件
+        EditText et_info = (EditText) findViewById(R.id.et_info);
+        Button btn_read = (Button) findViewById(R.id.btn_read);
+        Button btn_save = (Button) findViewById(R.id.btn_save);
+    }
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            //意图实现跳转Activity
-            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-            startActivity(intent);
+    //定义button按钮的点击事件
+    private abstract class ButtonListener implements CancellationSignal.OnCancelListener {
+        @SuppressLint("WrongConstant")
+        public void onClick(View v) throws IOException {
+            switch (v.getId()) {
+                case R.id.btn_save:
+                    String saveinfo = et_info.getText().toString().trim();
+                    FileOutputStream fos;
+                    try {
+                        //保存数据
+                        fos = openFileOutput("data.txt", Context.MODE_APPEND);
+                        fos.write(saveinfo.getBytes());
+                        fos.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this,"数据保存成功",0).show();
+                    break;
+                case R.id.btn_read:
+                    String content = "";
+                    try {
+                        //获取保存的数据
+                        FileInputStream fis = openFileInput("data.txt");
+                        byte[] buffer = new byte[fis.available()];
+                        fis.read(buffer);
+                        content = new String(buffer);
+                        fis.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this, "保存的数据是：" + content, 0).show();
+                    break;
+                default:
+                    break;
+            }
         }
-       });
-
+    }
 }
-}
-
-
-
-
