16cfea399eaa71f8ff5d3a856f7fee7bada3a120
ReiujiTenshi <592130546@q.com>
2019-04-19 18:33:05 +0800

    #4 #1052 提交实验四代码

---
+++ TouhouProject/app/src/main/java/com/example/gdy/soft1714080902318touhouproject/Soft1714080902318MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.gdy.soft1714080902318touhouproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.gdy.touhouproject.R;
+
+public class Soft1714080902318MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902318_main);
+
+        textView = (TextView)findViewById(R.id.button1);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+        @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(Soft1714080902318MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+}
---
+++ TouhouProject/app/src/main/java/com/example/gdy/soft1714080902318touhouproject/ThirdActivity.java
@@ -0,0 +1,15 @@
+package com.example.gdy.soft1714080902318touhouproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gdy.touhouproject.R;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
