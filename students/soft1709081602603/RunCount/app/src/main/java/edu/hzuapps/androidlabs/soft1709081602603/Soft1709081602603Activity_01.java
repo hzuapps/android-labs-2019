94ca6c1191ca48b2f09c0b932adca61138e92bc9
luxiaoshanhhh <2682786040@qq.com>
2019-04-19 16:01:59 +0800

    实验六代码

--- students/soft1709081602603/RunCount/app/src/main/java/edu/hzuapps/androidlabs/soft1709081602603/Soft1709081602603Activity_01.java
+++ students/soft1709081602603/RunCount/app/src/main/java/edu/hzuapps/androidlabs/soft1709081602603/Soft1709081602603Activity_01.java
@@ -7,21 +7,17 @@ import android.widget.Button;
 import android.view.View;
 
 public class Soft1709081602603Activity_01 extends AppCompatActivity implements View.OnClickListener {
-    private Button button1,button2;
+    private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1709081602603_activity_01);
         button1= (Button) findViewById(R.id.button1);
-        button2= (Button) findViewById(R.id.button2);
         button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
     }
     @Override
     public void onClick(View v){
         Intent intent=new Intent(Soft1709081602603Activity_01.this,Soft1709081602603Activity_02.class);
-        Intent intent1=new Intent(Soft1709081602603Activity_01.this,Soft1709081602603SetActivity.class);
         startActivity(intent);
-        startActivity(intent1);
     }
 }
