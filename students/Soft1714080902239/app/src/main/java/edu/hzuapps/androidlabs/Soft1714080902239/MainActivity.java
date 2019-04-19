b615e51fed940d9d3ae39028ecc70c9cabaf1772
ridworld <48148324+ridworld@users.noreply.github.com>
2019-04-20 00:52:43 +0800

    #实验七

--- students/Soft1714080902239/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902239/MainActivity.java
+++ students/Soft1714080902239/app/src/main/java/edu/hzuapps/androidlabs/Soft1714080902239/MainActivity.java
@@ -1,21 +1,71 @@
 package edu.hzuapps.myapplication;
 
-import android.os.Handler;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
-    private  Handler handler;
-    private  ImageView imageView;
+    public static final int TAKE_PHOTO=1;
+    private ImageView picture;
+    private Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button takePhoto=(Button)findViewById(R.id.photo);
+        picture =(ImageView)findViewById(R.id.picture);
 
-        imageView =(ImageView)findViewById(R.id.imageview);
-        handler = new Handler();
-        new HttpThread("https://github.com/ridworld/android-labs-2019/blob/master/students/Soft1714080902239/app/src/main/res/drawable/xiaoxin.jpg", imageView, handler).start();
-
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File outputImage=new File(getExternalCacheDir(),"output_image.jpg");
+                try{
+                    if (outputImage.exists()){
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT>=24){
+                    imageUri= FileProvider.getUriForFile(MainActivity.this,"edu.hzuapps.myapplication.fileprovide",outputImage);
+                }else{
+                    imageUri=Uri.fromFile(outputImage);
+                }
+                Intent intent =new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(intent,TAKE_PHOTO);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        switch (requestCode){
+            case TAKE_PHOTO:
+                if(requestCode==RESULT_OK){
+                    try{
+                        Bitmap bitmap= BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
