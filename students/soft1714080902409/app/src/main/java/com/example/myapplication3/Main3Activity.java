3affe9984d729ee243704d5a78a33601a37aeafa
liutao2019 <1980661451@qq.com>
2019-04-19 20:26:28 +0800

    #6 #Sakuraxia 第六次实验

--- students/soft1714080902409/app/src/main/java/com/example/myapplication3/Main3Activity.java
+++ students/soft1714080902409/app/src/main/java/com/example/myapplication3/Main3Activity.java
@@ -1,80 +1,82 @@
 package com.example.myapplication3;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.StrictMode;
-import android.text.TextUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
-
+import android.widget.Button;
+import android.widget.TextView;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class Main3Activity extends Activity {
-    protected static final int CHANGE_UI=1;
-    protected static final int ERROR=2;
-    private EditText et_path;
-    private ImageView iv;
-    private Handler handler=new Handler() {
-        public void handleMessage(android.os.Message msg){
-            if(msg.what==CHANGE_UI){
-                Bitmap bitmap=(Bitmap) msg.obj;
-                iv.setImageBitmap(bitmap);
-            }else if(msg.what==ERROR){
-                Toast.makeText(Main3Activity.this,"没有你要找的失物",Toast.LENGTH_LONG).show();
-            }
-        };
-    };
+public class Main3Activity extends AppCompatActivity {
+    private TextView et_path;
+    private Button bu;
+    TextView textView=null;
+    String text=null;
+    JSONObject object;
+    String URL = "https://raw.githubusercontent.com/Sakuraxia/android-labs-2019/master/data1/新建文本文档%20.JSON";
+    @Override
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-        et_path=(EditText) findViewById(R.id.et_path);
-        iv=(ImageView) findViewById(R.id.iv);
-    }
-    public void click(View view){
-        final String path=et_path.getText().toString().trim();
-        if(TextUtils.isEmpty(path)){
-            Toast.makeText(this,"失物的图片路径不能为空",Toast.LENGTH_LONG).show();
-        }else{
-            new Thread(){
-                private HttpURLConnection conn;
-                private Bitmap bitmap;
-                public void run(){
-                    try{
-                        URL url=new URL(path);
-                        conn=(HttpURLConnection) url.openConnection();
-                        conn.setRequestMethod("GET");
-                        conn.setConnectTimeout(5000);
-                        conn.setRequestProperty("User-Agent","Mozilla/4.0(compatible;MSIE 6.0;Windows NT 5.1;"+"SV1;.NET4.0C;.NET4.0E;.NET CLR 2.0.50727;"+".NET CLR 3.0.4506.2152;.NET CLR 3.5.30729;Shuame)");
-                        int code=conn.getResponseCode();
-                        if(code==200){
-                            InputStream is=conn.getInputStream();
-                            bitmap=BitmapFactory.decodeStream(is);
-                            Message msg=new Message();
-                            msg.what=CHANGE_UI;
-                            msg.obj=bitmap;
-                            handler.sendMessage(msg);
-                        }else{
-                            Message msg=new Message();
-                            msg.what=ERROR;
-                            handler.sendMessage(msg);
+        textView=findViewById(R.id.et_path);
+        bu=findViewById(R.id.bu);
+        Button bubu=(Button)findViewById(R.id.bu);
+        bubu.setOnClickListener(new View.OnClickListener(){
+            public void getjson(){
+                new Thread(new Runnable(){
+                    @Override
+                    public void run(){
+                        try{
+                            java.net.URL url =new URL(URL);
+                            HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+                            connection.setRequestMethod("GET");
+                            connection.setConnectTimeout(5000);
+                            connection.setReadTimeout(5000);
+                            connection.setUseCaches(false);
+                            connection.connect();
+                            InputStream in=connection.getInputStream();
+                            InputStreamReader input=new InputStreamReader(in);
+                            BufferedReader reader=new BufferedReader(input);
+                            if (connection.getResponseCode()==200) {
+                                StringBuilder response = new StringBuilder();
+                                String Line;
+                                while ((Line = reader.readLine()) != null) {
+                                    response.append(Line);
+                                }
+                                text = response.toString();
+                                System.out.println(text);
+                                object = new JSONObject(text);
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
                         }
-                    }catch(Exception e){
-                        e.printStackTrace();
-                        Message msg=new Message();
-                        msg.what=ERROR;
-                        handler.sendMessage(msg);
+                        runOnUiThread(new Runnable() {
+                            @SuppressLint("SetText")
+                            @Override
+                            public void run() {
+                                try{
+                                    bu.setText(object.getString("songer"));
+                                }catch(JSONException e){
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
                     }
-                };
-            }.start();
-        }
+                }).start();
+            }
+            @Override
+            public void onClick(View v){
+                getjson();
+            }
+        });
     }
+
 }
