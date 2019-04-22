d0d93be1d2b5ee239efd71c3d87c88326447b5e9
user <2755138625@qq.com>
2019-04-22 23:29:55 +0800

    #5 #596 提交实验5代码

--- students/soft1714080902135/app/src/main/java/edu/hzuapps/androidlabs/lsm/soft1714080902135/OneclickRegistrationActivity.java
+++
@@ -1,7 +0,0 @@
-package edu.hzuapps.androidlabs.lsm.soft1714080902135;
-
-
-
-public class OneclickRegistrationActivity{
-
-}
--- students/soft1714080902135/app/src/main/java/edu/hzuapps/androidlabs/lsm/soft1714080902135/ReportQueryActivity.java
+++
@@ -1,4 +0,0 @@
-package edu.hzuapps.androidlabs.lsm.soft1714080902135;
-
-public class ReportQueryActivity {
-}
