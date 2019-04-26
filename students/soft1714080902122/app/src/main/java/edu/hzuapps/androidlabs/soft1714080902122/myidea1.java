66e3172ca756b9b3af76aa0af56f7b0e3b4dfea1
wqrhome <317449116@qq.com>
2019-04-26 23:58:51 +0800

    第六次实验

--- students/soft1714080902122/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902122/myidea1.java
+++ students/soft1714080902122/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902122/myidea1.java
@@ -1,13 +1,24 @@
 package edu.hzuapps.androidlabs.soft1714080902122;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class myidea1 extends AppCompatActivity {
-    private Button button;
+    Button button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +32,8 @@ public class myidea1 extends AppCompatActivity {
                 startActivity(intent);            }
         });
 
+
+
     }
-}
+    }
+
