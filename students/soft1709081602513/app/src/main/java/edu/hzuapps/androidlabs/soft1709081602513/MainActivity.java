5dc33735d328b63f723a382469173f058647ca34
1085852964 <1085852964@qq.com>
2019-04-26 21:11:13 +0800

    #7 #491 第七次实验 重力感应旋转屏幕

--- students/soft1709081602513/app/src/main/java/edu/hzuapps/androidlabs/soft1709081602513/MainActivity.java
+++ students/soft1709081602513/app/src/main/java/edu/hzuapps/androidlabs/soft1709081602513/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.hzuapps.androidlabs.soft1709081602513;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +9,18 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     private Button  mBtnSetting;
     private ImageButton mBtnStart;
     private TextView xiaView;
+    //圆图片
+    private TextView circleimageview;
+    //路径
+    private Uri uri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
         mBtnStart.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void  onClick(View v){
+            public void onClick(View v) {
                 //跳转到开始界面StartActivity
-                Intent intent = new Intent(MainActivity.this,StartActivity.class);
+                Intent intent = new Intent(MainActivity.this, StartActivity.class);
                 startActivity(intent);
 
             }
@@ -37,9 +45,9 @@ public class MainActivity extends AppCompatActivity {
         //设置触摸监听器
         mBtnSetting.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void  onClick(View view){
+            public void onClick(View view) {
                 //跳转到设置界面SettingsActivity
-                Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
                 startActivity(intent);
             }
         });
@@ -48,15 +56,24 @@ public class MainActivity extends AppCompatActivity {
         //设置触摸监听器
         xiaView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void  onClick(View v){
+            public void onClick(View v) {
                 //跳转到Sof1709081602513Activity界面
-                Intent intent = new Intent(MainActivity.this,Soft1709081602513Activity.class);
+                Intent intent = new Intent(MainActivity.this, Soft1709081602513Activity.class);
                 startActivity(intent);
             }
         });
+        //调用初始化用户名
+        Initname();
+
 
     }
-
-
+    //初始化用户名
+    public void Initname(){
+        SharedPreferences pref=getSharedPreferences("config",MODE_PRIVATE);
+        String name=pref.getString("username","");
+        if(name!=null){
+            xiaView.setText(name);
+        }
+    }
 
 }
--- students/soft1709081602513/app/src/main/java/edu/hzuapps/androidlabs/soft1709081602513/StartActivity.java
+++ students/soft1709081602513/app/src/main/java/edu/hzuapps/androidlabs/soft1709081602513/StartActivity.java
@@ -1,25 +1,76 @@
 package edu.hzuapps.androidlabs.soft1709081602513;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.SensorManager;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.style.BackgroundColorSpan;
 import android.util.AttributeSet;
+import android.view.WindowManager;
 import android.widget.GridView;
 import android.widget.TextView;
 
+
+
 public class StartActivity extends AppCompatActivity {
-
-
-    private TextView mCave;
+    final String LOG = "------Screen------" ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
 //            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 //        }//使横屏
-        setContentView(R.layout.activity_start);
-        mCave= findViewById(R.id.cave);
+
+        // 应用运行时，保持屏幕高亮，不锁屏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        // 全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN) ;
+        //Activity的方向由方向传感器来决定，显示会根据用户设备的移动情况来旋转
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+//        setContentView(R.layout.activity_start);
+
+        if (savedInstanceState == null) {
+            setContentView(R.layout.activity_start);
+        }
+        //检测是横屏还是竖屏
+        if(savedInstanceState != null){
+            //若为横屏
+            if( ScreenOrient(this)==ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE  )
+                setContentView(R.layout.activity_start_landscape);
+            //竖屏
+            if( ScreenOrient(this)==ActivityInfo.SCREEN_ORIENTATION_PORTRAIT  )
+                setContentView(R.layout.activity_start);
+            String temp = savedInstanceState.getString("data_key") ;
+            Log.d(LOG , "重新创建了Activity，之前保存的内容是"+temp) ;
+        }
     }
+
+
+    public int ScreenOrient(Activity activity)
+    {
+        int orient = activity.getRequestedOrientation();
+        if(orient != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE && orient != ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+            WindowManager windowManager = activity.getWindowManager();
+            Display display = windowManager.getDefaultDisplay();
+            int screenWidth  = display.getWidth();
+            int screenHeight = display.getHeight();
+            orient = screenWidth < screenHeight ?  ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE:ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+        }
+        return orient;
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String tempData = "Something you want to save";
+        outState.putString("data_key", tempData);
+        Log.v(LOG, "onSaveInstanceState..");
+    }
+
 }
