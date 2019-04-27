a48d1a6eb4f4d3714db871c249a9f3a2eb63729b
YZT6 <1765422809@qq.com>
2019-04-27 22:02:24 +0800

    回退文件

--- students/soft1714080902240/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902240/Soft1714080902240Activity3.java
+++ students/soft1714080902240/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902240/Soft1714080902240Activity3.java
@@ -1,41 +1,43 @@
 package edu.hzuapps.androidlabs.soft1714080902240;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Soft1714080902240Activity3 extends AppCompatActivity {
     private TextView edittext;
+
     public static final String DIRECTORY = "demo";
     public static final String FILENAME = "file_demo.txt";
     public static final String TAG = Soft1714080902240Activity3.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft17140809022403);
 
-        boolean conn = ConnectionUtil.isConn(Soft1714080902240Activity3.this);
-        //如果没有网络
-        if (!conn) {
-            //跳转到设置网络的界面
-            ConnectionUtil.setNetworkMethod(Soft1714080902240Activity3.this);
-        } else {
-        }
 
         edittext = (TextView) findViewById(R.id.danci);
         Button button = (Button) findViewById(R.id.btn_save);
+
         button.setOnClickListener(new MyonclickListener());
     }
 
     private class MyonclickListener implements View.OnClickListener {
+
         @Override
         public void onClick(View v) {
             String string = edittext.getText().toString();
@@ -45,27 +47,36 @@ public class Soft1714080902240Activity3 extends AppCompatActivity {
 
     private void saveTextIntoInternalStorage(String text) {
         // 获取内部存储目录
-        File dir = this.getFilesDir();
+                File dir = this.getFilesDir();
+        //        //File dir = getCacheDir();
+
         if (dir.isDirectory()) {
 //            dir.mkdir();
 //            dir.mkdirs();
         }
 
-            if (dir.isFile()) {
+        if (dir.isFile()) {
             // D:/Abc.txt , -> D:/Abc1.txt ->D:/abc.txt
         }
 
         File file = new File(dir, FILENAME);
 
+   //     try {
+     //      File = File.createTempFile(FILENAME, null, dir);
+    //   } catch (IOException e) {
+    //        e.printStackTrace();
+    //   }
+
         if (file.exists()) { // 判断文件是否存在
-                Log.i(TAG, file.getAbsolutePath());
-                Log.i(TAG, file.length() + ""); // bytes*1024=kb *1024 MB
-                Log.i(TAG, file.isFile() + "");
-                file.canRead();
-                file.canWrite();
-                file.canExecute();
-                file.getFreeSpace();
-                file.getTotalSpace();
+            Log.i(TAG, file.getAbsolutePath());
+            Log.i(TAG, file.length() + ""); // bytes*1024=kb *1024 MB
+            Log.i(TAG, file.isFile() + "");
+            file.canRead();
+            file.canWrite();
+            file.canExecute();
+
+            file.getFreeSpace();
+            file.getTotalSpace();
         }
 
         FileOutputStream fos = null;  // 字节流  | char | cn : gbk 2 bytes, utf8 3 bytes
@@ -86,6 +97,9 @@ public class Soft1714080902240Activity3 extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+
         Toast.makeText(Soft1714080902240Activity3.this,"保存成功", 0).show();
     }
+
+
 }
