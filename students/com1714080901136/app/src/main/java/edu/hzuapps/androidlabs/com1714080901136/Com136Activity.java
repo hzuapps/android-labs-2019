dac6dd9a68284d56be2add18b991f94359edbbea
lxj2019 <1259789523@qq.com>
2019-04-28 20:32:04 +0800

    #7#136

--- students/com1714080901136/app/src/main/java/edu/hzuapps/androidlabs/com1714080901136/Com136Activity.java
+++ students/com1714080901136/app/src/main/java/edu/hzuapps/androidlabs/com1714080901136/Com136Activity.java
@@ -1,23 +1,30 @@
 package edu.hzuapps.androidlabs.com1714080901136;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Com136Activity extends AppCompatActivity {
-
+    public final int TAKE_PHOTO = 1;
+    public final int CHOOSE_PHOTO = 2;
+    public final int CROP_REQUEST_CODE = 3;
+    public ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com136);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
+        iv =  findViewById(R.id.iv);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,6 +44,93 @@ public class Com136Activity extends AppCompatActivity {
 
         startActivity(intent);
 
+    }public void chose(View v) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, CHOOSE_PHOTO);
     }
+    public void photograph(View v) {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, TAKE_PHOTO);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (data != null) {
+                    Uri mImageCaptureUri = data.getData();
+                    if (mImageCaptureUri != null) {
+                        Bitmap image;
+                        try {
+                            image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mImageCaptureUri);
+                            if (image != null) {
+                                iv.setImageBitmap(image);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Bundle extras = data.getExtras();
+                        if (extras != null) {
+                            Bitmap image = extras.getParcelable("data");
+                            if (image != null) {
+                                iv.setImageBitmap(image);
+                            }
+                        }
+                    }
+
+                }
+                break;
+            case CROP_REQUEST_CODE:
+                if (data != null) {
+                    Uri mImageCaptureUri = data.getData();
+                    if (mImageCaptureUri != null) {
+                        Bitmap image;
+                        try {
+                            image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mImageCaptureUri);
+                            if (image != null) {
+                                iv.setImageBitmap(image);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Bundle extras = data.getExtras();
+                        if (extras != null) {
+                            Bitmap image = extras.getParcelable("data");
+                            if (image != null) {
+                                iv.setImageBitmap(image);
+                            }
+                        }
+                    }
+
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    cropPhoto(uri);//裁剪
+                }
+                break;
+        }}
+    private void cropPhoto(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri,"image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 2);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 400);//剪裁后X的像素
+        intent.putExtra("outputY", 200);//剪裁后Y的像素
+        intent.putExtra("outputFormat", "JPEG");// 返回格式
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, CROP_REQUEST_CODE);
+
+    }
+
 
 }
