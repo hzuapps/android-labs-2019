1b54c158de5244e26e582f7387527c6594f29f9d
loker111 <874886288@qq.com>
2019-04-28 20:53:41 +0800

    第一次实验

---
+++ students/soft1714080902140/java/edu/hzuapps/androidlabs/soft1714080902140/Soft1714080902140Activity.java
@@ -0,0 +1,9 @@
+package edu.hzuapps.androidlabs.soft1714080902140;
+
+public class Soft1714080902140Activity {
+
+    public static void  main(String[] args){
+        System.out.println("hello world");
+    }
+
+}
