2a539689279d0a4d5f24edbc9e943e8359e2e380
nekodevelopers <48147997+nekodevelopers@users.noreply.github.com>
2019-04-28 12:02:57 +0800

    Update new_activity.java

--- students/soft1714080902233/实验3/Pic/app/src/main/java/com/example/administrator/pic/new_activity.java
+++ students/soft1714080902233/实验3/Pic/app/src/main/java/com/example/administrator/pic/new_activity.java
@@ -55,8 +55,8 @@ public class new_activity extends AppCompatActivity {
                 if (!outputImage.getParentFile().exists()){
                     outputImage.getParentFile().mkdirs();
                 }
-                if (Build.VERSION.SDK_INT >= 24) {
-                    imageUri = FileProvider.getUriForFile(new_activity.this, "com.example.administrator.pic.fileprovider", outputImage);
+                if (Build.VERSION.SDK_INT >= 24) {//测试机版本是8.0
+                    imageUri = FileProvider.getUriForFile(new_activity.this, "com.example.administrator.pic.fileprovider", outputImage);//使用fileprovider创建图片的uri
                 } else {
                     Toast.makeText(new_activity.this,"version is to low", Toast.LENGTH_LONG).show();
                 }
@@ -98,7 +98,7 @@ public class new_activity extends AppCompatActivity {
                 "        \"http\": \"https://blog.csdn.net/weixin_41544329\",\n" +
                 "        \"emotion\": \"（＝。＝）\"\n" +
                 "    }";
-        final javabean_web bean=gson.fromJson(csdnjson,javabean_web.class);
+        final javabean_web bean=gson.fromJson(csdnjson,javabean_web.class);//我使用gson解析的时候要对应一个javabean
         Button csdn=(Button) findViewById(R.id.Skip_to_csdn);
         csdn.setOnClickListener(
                 new View.OnClickListener()
@@ -126,7 +126,7 @@ public class new_activity extends AppCompatActivity {
         list_path.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555432842011&di=2c632c93ae289336571c1d3366665779&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Ff48dde221d1c755653696f4f8fe43b7063faf5dc306e97-CazFAe_fw236");
         list_path.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555432839435&di=1e76cc7271baf30796a97b9840321c37&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201703%2F23%2F20170323174003_PY8cs.thumb.224_0.jpeg");
         list_path.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555432836624&di=296cb48ffd6b8202af6c88c86f55f57b&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fef833549e93f58ff30866eff432d26ba5338a6f72d335d-G2Iuai_fw236");
-        list_path.add("file://"+Environment.getExternalStorageDirectory()+"/DCIM/Camera/"+"test" + ".jpg");
+        list_path.add("file://"+Environment.getExternalStorageDirectory()+"/DCIM/Camera/"+"test" + ".jpg");//将拍摄的图片加入图片轮播集合里面
         list_title.add("一");
         list_title.add("二");
         list_title.add("三");
