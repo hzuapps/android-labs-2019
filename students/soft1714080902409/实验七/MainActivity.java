4ef17cdb80caa498d03b71897574181f8270e7d7
liutao2019 <1980661451@qq.com>
2019-05-03 18:40:50 +0800

    #7 #584 第七次实验

---
+++ students/soft1714080902409/实验七/MainActivity.java
@@ -0,0 +1,365 @@
+﻿package com.example.myapplication;
+
+        import android.annotation.TargetApi;
+
+        import android.content.ContentUris;
+
+        import android.content.Intent;
+
+        import android.database.Cursor;
+
+        import android.graphics.Bitmap;
+
+        import android.graphics.BitmapFactory;
+
+        import android.net.Uri;
+
+        import android.os.Build;
+
+        import android.os.Environment;
+
+        import android.provider.DocumentsContract;
+
+        import android.provider.MediaStore;
+
+        import android.support.v7.app.AppCompatActivity;
+
+        import android.os.Bundle;
+
+        import android.view.View;
+
+        import android.widget.Button;
+
+        import android.widget.ImageView;
+
+        import android.widget.Toast;
+
+
+
+        import java.io.File;
+
+        import java.io.FileNotFoundException;
+
+        import java.io.IOException;
+
+
+
+public class  MainActivity extends AppCompatActivity {
+
+
+
+    public static final int TAKE_PHOTO = 1;
+
+    public static final int CROP_PHOTO = 2;
+
+    public static final int CHOOSE_PHOTO = 3;
+
+    private Button takephoto;
+
+    private Button choosephoto;
+
+    private ImageView picture;
+
+    private Uri imageUri;
+
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        takephoto = (Button) findViewById(R.id.take_photo);
+
+        picture = (ImageView) findViewById(R.id.picture);
+
+        choosephoto = (Button) findViewById(R.id.choose_photo);
+
+
+        takephoto.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+    
+
+                File outputImage = new File(Environment.getExternalStorageDirectory(),
+
+                        "output_image.jpg");
+
+      
+
+                try {
+
+                    if (outputImage.exists()) {
+
+                        outputImage.delete();
+
+                    }
+
+                    outputImage.createNewFile();
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+
+                }
+
+     
+                imageUri = Uri.fromFile(outputImage);
+
+        
+
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+          
+
+                startActivityForResult(intent, TAKE_PHOTO);
+
+            }
+
+        });
+
+
+
+        choosephoto.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent("android.intent.action.GET_CONTENT");
+
+                intent.setType("image/*");
+
+
+                startActivityForResult(intent, CHOOSE_PHOTO);
+
+            }
+
+        });
+
+    }
+
+
+
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+
+
+
+            case TAKE_PHOTO:
+
+                if (resultCode == RESULT_OK) {
+
+                    Intent intent = new Intent("com.android.camera.action.CROP");
+
+                    intent.setDataAndType(imageUri, "image/*");
+
+                    intent.putExtra("scale", true);
+
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+
+
+                    startActivityForResult(intent, CROP_PHOTO);
+
+                }
+
+                break;
+
+
+
+            case CROP_PHOTO:
+
+                if (resultCode == RESULT_OK) {
+
+                    try {
+
+                        Bitmap bitmap = BitmapFactory.decodeStream(
+
+                                getContentResolver().openInputStream(imageUri));
+
+                        picture.setImageBitmap(bitmap); //显示剪裁后的图片
+
+                    } catch (FileNotFoundException e) {
+
+                        e.printStackTrace();
+
+                    }
+
+                }
+
+                break;
+
+
+
+            case CHOOSE_PHOTO:
+
+                if (resultCode == RESULT_OK) {
+
+                    if (Build.VERSION.SDK_INT >= 19) {
+
+        
+
+                        handleImageOnKitKat(data);
+
+                    } else {
+
+        
+
+                        handleImageBeforeKitKat(data);
+
+                    }
+
+                }
+
+                break;
+
+            default:
+
+                break;
+
+        }
+
+    }
+
+
+
+
+
+    @TargetApi(19)
+
+    private void handleImageOnKitKat(Intent data) {
+
+        String imagePath = null;
+
+   
+
+        Uri uri = data.getData();
+
+ 
+
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+
+            String docId = DocumentsContract.getDocumentId(uri);
+
+ 
+            if ("com.android.providers.media.documents".equals(
+
+                    uri.getAuthority())) {
+
+                String id = docId.split(":")[1];
+
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+
+       
+
+                imagePath = getImagePath(
+
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+
+            } else if ("com.android.providers.downloads.documents".equals(
+
+                    uri.getAuthority())) {
+
+
+                Uri contentUri = ContentUris.withAppendedId(
+
+                        Uri.parse("content://downloads/public_downloads"),
+
+                        Long.valueOf(docId));
+
+                imagePath = getImagePath(contentUri, null);
+
+            }
+
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+     
+
+            imagePath = getImagePath(uri, null);
+
+        }
+
+ 
+        displayImage(imagePath);
+
+    }
+
+
+
+ 
+
+    private void handleImageBeforeKitKat(Intent data) {
+
+        Uri uri = data.getData();
+
+        String imagePath = getImagePath(uri, null);
+
+        displayImage(imagePath);
+
+    }
+
+
+
+
+
+    private String getImagePath(Uri uri, String selection) {
+
+        String path = null;
+
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+
+        if (cursor != null) {
+
+     
+
+            if (cursor.moveToFirst()) {
+
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+
+            }
+
+     
+
+            cursor.close();
+
+        }
+
+        return path;
+
+    }
+
+
+
+
+    private void displayImage(String imagePath) {
+
+        if (imagePath != null) {
+
+
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+
+            picture.setImageBitmap(bitmap);
+
+        } else {
+
+            Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+}
+
+
