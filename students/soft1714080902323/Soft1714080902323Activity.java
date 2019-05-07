7d493944544083c5484b466c085f22397812c712
Edisss <48149810+Edisss@users.noreply.github.com>
2019-05-07 10:08:25 +0800

    提交第8次实验

--- students/soft1714080902323/Soft1714080902323Activity.java
+++ students/soft1714080902323/Soft1714080902323Activity.java
@@ -2,14 +2,28 @@ package androidlabs2019.students.soft1714080902323;
 
 import android.app.Activity;
 import android.app.AppComponentFactory;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class Soft1714080902323Activity extends Activity {
+    private TextView textView;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.soft_1714080902323_activity1);
-    }
+        setContentView(R.layout.soft_1714080902323_activity);
+        textView = (TextView) findViewById(R.id.textview_01);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Soft1714080902323Activity.this, Soft1714080902323Activity1.class);
+                startActivity(intent);
+            }
+        });
 
+    }
 }
--- students/soft1714080902323/Soft1714080902323Activity1.java
+++ students/soft1714080902323/Soft1714080902323Activity1.java
@@ -10,23 +10,39 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Soft1714080902323Activity1 extends Activity {
-    private Button button;
+    private Button button1;
+    private Button button2;
+    private Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1714080902323_activity1);
-        button= (Button) findViewById(R.id.xinxibiao);
-        button.setOnClickListener(new View.OnClickListener() {
+        button1= (Button) findViewById(R.id.xinxibiao);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Soft1714080902323Activity1.this, Soft1714080902323Activity2.class);
-
-
                 startActivity(intent);
             }
         });
-
+        button2= (Button) findViewById(R.id.kechengbiao);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Soft1714080902323Activity1.this, Soft1714080902323Activity4.class);
+                startActivity(intent);
+            }
+        });
+        button3= (Button) findViewById(R.id.chengjibiao);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Soft1714080902323Activity1.this, Soft1714080902323Activity5.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
 
 
