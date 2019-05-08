3cb32700689102cb2cb4b1d76e609f3ce62d291d
kaluozi <784745401@qq.com>
2019-05-08 23:37:13 +0800

    #5 #541 提交实验六代码(选做二)

--- students/soft1714080902401/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902401/Soft1714080902401Activity.java
+++ students/soft1714080902401/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902401/Soft1714080902401Activity.java
@@ -4,15 +4,20 @@ package edu.hzuapps.androidlabs.soft1714080902401;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+
 
 public class Soft1714080902401Activity extends AppCompatActivity {
 
+    private static final String TAG = "tag";
     private ImageView mfpimage;
 
     @Override
@@ -21,7 +26,7 @@ public class Soft1714080902401Activity extends AppCompatActivity {
         setContentView(R.layout.activity_soft_1714080902401);
 
         mfpimage=(ImageView) findViewById(R.id.fpimage);
-        Glide.with(this).load("https://b-ssl.duitang.com/uploads/item/201903/17/20190317150323_opsor.thumb.700_0.jpg").into(mfpimage);
+        Glide.with(this).load("https://b-ssl.duitang.com/uploads/item/201903/17/20190317150323_opsor.thumb.700_0.jpg").error(R.drawable.errorview).into(mfpimage);
 
         Button begin = (Button) findViewById(R.id.Begin);
         begin.setOnClickListener(new View.OnClickListener() {
