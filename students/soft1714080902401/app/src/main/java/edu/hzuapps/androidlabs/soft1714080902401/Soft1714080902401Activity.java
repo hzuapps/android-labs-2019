2acafb99830b38b801a62b7caf8fe90141a1ee6e
kaluozi <784745401@qq.com>
2019-05-08 21:52:34 +0800

    #6 #541 提交实验六代码(选做二)

--- students/soft1714080902401/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902401/Soft1714080902401Activity.java
+++ students/soft1714080902401/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902401/Soft1714080902401Activity.java
@@ -6,14 +6,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 
 public class Soft1714080902401Activity extends AppCompatActivity {
+
+    private ImageView mfpimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft_1714080902401);
+
+        mfpimage=(ImageView) findViewById(R.id.fpimage);
+        Glide.with(this).load("https://b-ssl.duitang.com/uploads/item/201903/17/20190317150323_opsor.thumb.700_0.jpg").into(mfpimage);
 
         Button begin = (Button) findViewById(R.id.Begin);
         begin.setOnClickListener(new View.OnClickListener() {
