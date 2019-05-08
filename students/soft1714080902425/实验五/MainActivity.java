4bca6e822e045ce35a690962413e90f0876a4ba3
AngleBeatQAQ <499352824@qq.com>
2019-05-08 22:57:54 +0800

    #5 #1641 #第五次实验

--- students/soft1714080902425/实验五/MainActivity.java
+++ students/soft1714080902425/实验五/MainActivity.java
@@ -1,19 +1,15 @@
-package edu.androidlabs.thirdtest;
+﻿package edu.androidlabs.thirdtest;
 
 
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-
 import java.io.FileOutputStream;
-
 
 
 public class MainActivity extends AppCompatActivity {
