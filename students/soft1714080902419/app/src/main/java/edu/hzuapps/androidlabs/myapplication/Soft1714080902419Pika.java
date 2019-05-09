9f4918cf77c106efd9924f3fbef915611842ff55
hcwcc <1497330792@qq.com>
2019-05-09 21:07:02 +0800

    #5 #633 第五次实验

---
+++ students/soft1714080902419/app/src/main/java/edu/hzuapps/androidlabs/myapplication/Soft1714080902419Pika.java
@@ -0,0 +1,62 @@
+package edu.hzuapps.androidlabs.myapplication;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Soft1714080902419Pika extends AppCompatActivity {
+    private EditText et_info;
+    private Button button1;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1714080902419_pika);
+        et_info=(EditText) findViewById(R.id.et_info);
+        button1=(Button) findViewById(R.id.button1);
+        button2=(Button) findViewById(R.id.button2);
+        button1.setOnClickListener(new ButtonListener());
+        button2.setOnClickListener(new ButtonListener());
+    }
+    private class ButtonListener implements View.OnClickListener{
+        public void onClick(View v){
+            switch (v.getId()){
+                case R.id.button1:
+                    String saveinfo=et_info.getText().toString().trim();
+                    FileOutputStream fos;
+                    try{
+                        fos=openFileOutput("data.txt", Context.MODE_APPEND);
+                        fos.write(saveinfo.getBytes());
+                        fos.close();
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Soft1714080902419Pika.this,"备注成功",Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.button2:
+                    String content="";
+                    try{
+                        FileInputStream fis=openFileInput("data.txt");
+                        byte[] buffer=new byte[fis.available()];
+                        fis.read(buffer);
+                        content=new String(buffer);
+                        fis.close();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Soft1714080902419Pika.this,"备注为"+content,Toast.LENGTH_LONG).show();
+                    break;
+                    default:
+                        break;
+            }
+        }
+    }
+}
