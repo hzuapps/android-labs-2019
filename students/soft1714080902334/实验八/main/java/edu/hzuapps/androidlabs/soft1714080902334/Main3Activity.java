a5833820be1f25df35d325b3afe2c2bccb04b32d
= <=>
2019-05-10 01:28:46 +0800

    #8 大作业，实验八

---
+++ students/soft1714080902334/实验八/main/java/edu/hzuapps/androidlabs/soft1714080902334/Main3Activity.java
@@ -0,0 +1,47 @@
+package edu.hzuapps.androidlabs.soft1714080902334;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button3 = (Button) findViewById(R.id.button3);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main3Activity.this, SaveActivity.class);
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main3Activity.this, Json2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
