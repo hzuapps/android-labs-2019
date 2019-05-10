70be04ac815abf267e87cf4d907b829e0f9dee2f
hhyzz <1248019117@qq.com>
2019-05-10 12:17:56 +0800

    第二个页面的java文件

---
+++ students/soft1714080902203/shiyan8/MyApplication4/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902203/Soft1714080902203Activity1.java
@@ -0,0 +1,83 @@
+package edu.hzuapps.androidlabs.soft1714080902203;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Soft1714080902203Activity1 extends AppCompatActivity {
+    Button button = null;
+    private Button btn;
+    private ImageView imageview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1714080902203_activity1);
+
+
+        btn = (Button) this.findViewById(R.id.button);
+        imageview = (ImageView) this.findViewById(R.id.imageView);
+        button = findViewById(R.id.buttons);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(Soft1714080902203Activity1.this,Soft1714080902203Activity2.class);
+                startActivity(intent);
+            }
+        });
+        //连接网络的函数
+        StrictMode.setThreadPolicy(new
+                StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
+        StrictMode.setVmPolicy(
+                new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects().detectLeakedClosableObjects().penaltyLog().penaltyDeath().build());
+
+        btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                btn.setEnabled(false);
+                String strURL = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555611660563&di=4a380e6f6b2fc29ba4d29c496ea191e5&imgtype=0&src=http%3A%2F%2Ftvax1.sinaimg.cn%2Fcrop.0.0.1080.1080.1024%2Fe4669cc6ly8g1fkivhuujj20u00u0gov.jpg";
+                try {
+                    //将图片通过URL形式，用函数转换成图片显示出来
+                    Bitmap bitmap = getBitmap(strURL);
+                    imageview.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    // 检测异常
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    public Bitmap getBitmap(String path) throws IOException {
+        try {
+            URL url = new URL(path);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setRequestMethod("GET");
+            if (conn.getResponseCode() == 200) {
+                InputStream inputStream = conn.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            }
+        } catch (IOException e) {
+            // 检测异常
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
