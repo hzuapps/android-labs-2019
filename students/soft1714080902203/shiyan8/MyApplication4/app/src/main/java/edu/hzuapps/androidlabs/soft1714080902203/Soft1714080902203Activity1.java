57f325a114920ba7f585ae346957c4012fb32701
hhyzz <1248019117@qq.com>
2019-05-10 19:26:18 +0800

    修改后的第一个页面JAVA文件

--- students/soft1714080902203/shiyan8/MyApplication4/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902203/Soft1714080902203Activity1.java
+++ students/soft1714080902203/shiyan8/MyApplication4/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902203/Soft1714080902203Activity1.java
@@ -1,10 +1,13 @@
 package edu.hzuapps.androidlabs.soft1714080902203;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,9 +17,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Soft1714080902203Activity1 extends AppCompatActivity {
+    private EditText jcwu;
+    private Button btn_save;
+    private Button btn_read;
     Button button = null;
     private Button btn;
     private ImageView imageview;
@@ -25,6 +33,14 @@ public class Soft1714080902203Activity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1714080902203_activity1);
+
+        //获取布局文件中的控件
+        jcwu = (EditText) findViewById(R.id.jcwu);
+        btn_save = (Button) findViewById(R.id.btn_save);
+        btn_read = (Button) findViewById(R.id.btn_read);
+        //设置按钮点击函数
+        btn_save.setOnClickListener(new ButtonListener());
+        btn_read.setOnClickListener(new ButtonListener());
 
 
         btn = (Button) this.findViewById(R.id.button);
@@ -78,6 +94,44 @@ public class Soft1714080902203Activity1 extends AppCompatActivity {
             e.printStackTrace();
         }
         return null;
+    }
+
+
+    //定义Button按钮点击事件
+    private class ButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_save:
+                    String saveinfo = jcwu.getText().toString().trim();
+                    FileOutputStream fileOutputStream;
+                    try {
+                        fileOutputStream=openFileOutput("data.txt", Context.MODE_APPEND);
+                        fileOutputStream.write(saveinfo.getBytes());
+                        fileOutputStream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(Soft1714080902203Activity1.this,"以保存！",Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.btn_read:
+                    String content="";
+                    try{
+                        FileInputStream fileInputStream=openFileInput("data.txt");
+                        byte[] buffer=new byte[fileInputStream.available()];
+                        fileInputStream.read(buffer);
+                        content=new String(buffer);
+                        fileInputStream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }Toast.makeText(Soft1714080902203Activity1.this,"您的爱豆简介已保存："+content,Toast.LENGTH_SHORT).show();
+                    break;
+
+
+                default: break;
+            }
+        }
     }
 
 }
