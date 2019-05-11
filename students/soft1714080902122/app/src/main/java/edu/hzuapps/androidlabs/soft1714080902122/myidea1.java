4241cd654d32950fc9cbd29b23dc3d5daf615316
wqrhome <317449116@qq.com>
2019-05-11 21:15:20 +0800

    第八次实验

--- students/soft1714080902122/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902122/myidea1.java
+++ students/soft1714080902122/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902122/myidea1.java
@@ -1,5 +1,6 @@
 package edu.hzuapps.androidlabs.soft1714080902122;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,31 +10,71 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class myidea1 extends AppCompatActivity {
-    Button button;
 
+    private Button button;
+    private EditText et_info;
+    private Button btn_save;
+    private Button btn_read;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myidea1_activity);
 
-        button = (Button) findViewById(R.id.button_5);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(myidea1.this, SaveActivity.class);
-                startActivity(intent);            }
-        });
+        //获取布局文件中的控件
+        et_info = (EditText) findViewById(R.id.et_info);
+        btn_save = (Button) findViewById(R.id.btn_save);
+        btn_read = (Button) findViewById(R.id.btn_read);
+        btn_save.setOnClickListener(new myidea1.ButtonListener());
+        btn_read.setOnClickListener(new myidea1.ButtonListener());
+    }
 
-
-
+    //定义Button按钮的点击事件
+    private class ButtonListener implements View.OnClickListener {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_save:
+                    String saveinfo = et_info.getText().toString().trim();
+                    FileOutputStream fos;
+                    try {
+                        //保存数据
+                        fos = openFileOutput("data.txt", Context.MODE_APPEND);
+                        fos.write(saveinfo.getBytes());
+                        fos.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(myidea1.this, "数据保存成功", 0).show();
+                    break;
+                case R.id.btn_read:
+                    String content = "";
+                    try {
+                        //获取保存的数据
+                        FileInputStream fis = openFileInput("data.txt");
+                        byte[] buffer = new byte[fis.available()];
+                        fis.read(buffer);
+                        content = new String(buffer);
+                        fis.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(myidea1.this, "保存的数据是：" + content, 0).show();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
     }
 
