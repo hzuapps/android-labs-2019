ff4eda26f3f943aa227180474bcd2a861d5c4ee5
Game111fff <964851688@qq.com>
2019-05-11 22:39:05 +0800

    #1 #第1次的实验

---
+++ students/soft1714080902312/app9/Soft1714080902312Activity.java
@@ -0,0 +1,7 @@
+package edu.hzuapps.androidlabs.soft1714080902312;
+
+public class Soft1714080902312Activity {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
