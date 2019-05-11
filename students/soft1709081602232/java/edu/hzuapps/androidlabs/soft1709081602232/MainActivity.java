3d5ca8e5533b6fc149e91ce6450d9466eba0e0e4
unknown <you@example.com>
2019-05-11 14:49:04 +0800

    #7 实验7

--- students/soft1709081602232/java/edu/hzuapps/androidlabs/soft1709081602232/MainActivity.java
+++ students/soft1709081602232/java/edu/hzuapps/androidlabs/soft1709081602232/MainActivity.java
@@ -1,26 +1,74 @@
 package soft1709081602232.androidlabs.hzuapps.edu.myapplication;
 
+import android.app.MediaRouteButton;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v4.content.IntentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button button;
-
+    public static final int TAKE_PHOTO=1;
+    private ImageView picture;
+    private Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button takePhoto=(Button)findViewById(R.id.take_photo);
+        picture=(ImageView)findViewById(R.id.picture);
+        takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(i);
+                File outputImage=new File(getExternalCacheDir(),"output_image.jpg");
+                try{
+                    if(outputImage.exists()){
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+                if(Build.VERSION.SDK_INT>=24){
+                    imageUri=FileProvider.getUriForFile(MainActivity.this,"com.example.cameraalbumtext.fileprovider",outputImage);
+
+                }else{
+                    imageUri=Uri.fromFile(outputImage);
+
+                }
+                Intent intent=new Intent("android.media.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(intent,TAKE_PHOTO);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data ){
+        switch (requestCode){
+            case TAKE_PHOTO:
+                if (resultCode==RESULT_OK){
+                    try{
+                        Bitmap bitmap=BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    }catch(FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
