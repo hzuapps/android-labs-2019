a2e380425d525c3be31ed0a66232aacf13195ab6
Yemingrui <3162548470@qq.com>
2019-05-16 21:40:17 +0800

    #4 #1898 实验四

---
+++ students/MyApplication/app/src/main/java/edu/hzuapps/androidlabs/soft1714080902338/myapplication/Main2Activity.kt
@@ -0,0 +1,12 @@
+package edu.hzuapps.androidlabs.soft1714080902338.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
---
+++ students/soft1714080902338/app/src/main/java/java/soft1714080902338activity/soft1714080902338/androidlabs/hzuapps/myapplication/MainActivity2.java
@@ -0,0 +1,13 @@
+package java.soft1714080902338activity.soft1714080902338.androidlabs.hzuapps.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
