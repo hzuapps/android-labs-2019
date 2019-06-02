563a2e64de5d9037883114d7d087feb7757f6fa6
lengyuanfeixue <48351258+lengyuanfeixue@users.noreply.github.com>
2019-06-02 17:28:54 +0800

    实验2

---
+++ students/soft1714080902236/otherActivity.java
@@ -0,0 +1,38 @@
+package com.example.shiyan33;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+
+
+public class otherActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otherxml);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);   //全屏
+
+        play_mp4();
+
+    }
+
+    private void play_mp4(){
+        String videoUrl1 = "/storage/emulated/0/123.mp4";
+        Uri uri = Uri.parse( videoUrl1 );
+        videoView = (VideoView)this.findViewById(R.id.videoView );
+        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoURI(uri);
+        videoView.start();
+    }
+
+}
+
+
