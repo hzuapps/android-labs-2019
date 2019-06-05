9b4270fdbf8a3ead5fabc1af466a4d19ab102ef0
Android-lgw <934424456@qq.com>
2019-06-05 23:47:05 +0800

    #5 #279 实验五代码

---
+++ students/soft1714080902325/app5/src/main/java/edu/hzuapps/androidlabs/soft1714080902325/HomeActivity.java
@@ -0,0 +1,28 @@
+package edu.hzuapps.androidlabs.soft1714080902325;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+    private Button button02;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    button02=(Button)findViewById(R.id.button2);
+        button02.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent(HomeActivity.this, CunchuActivity.class);
+            startActivity(intent);
+        }
+    });
+    }
+}
+
+
