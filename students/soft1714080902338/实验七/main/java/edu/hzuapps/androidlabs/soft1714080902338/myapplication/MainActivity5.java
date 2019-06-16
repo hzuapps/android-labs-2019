41b375ae033f5c106e4a34e483545a0c49d2bcf1
Yemingrui <3162548470@qq.com>
2019-06-16 23:16:39 +0800

    #7 #1898 实验七

---
+++ students/soft1714080902338/实验七/main/java/edu/hzuapps/androidlabs/soft1714080902338/myapplication/MainActivity5.java
@@ -0,0 +1,154 @@
+package edu.hzuapps.androidlabs.soft1714080902338.myapplication;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+
+public class MainActivity5 extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = MainActivity5.class.getSimpleName();
+    public static final String WEB = "http://www.bing.com";
+    public static final String IMAGE_URL_PREFIX = "https://raw.githubusercontent.com/AngleBeatQAQ/android-labs-2019/master/students/soft1714080902425/实验三/drawable/";
+
+    static String[] imageNames = {"play.png"};
+
+    private Button mCheckButton;
+    private Button mDownloadImageButton;
+    private Button mDownloadWebButton;
+    private TextView mNetworkText;
+    private TextView mResultText;
+    private boolean mConnected;
+    private MainActivity6 mFileDownloader;
+    private ImageView mimageView;
+    // App的内部存储目录
+    private File mPrivateRootDir;
+    // “images”子目录
+    private File mImagesDir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+
+        mCheckButton = (Button) findViewById(R.id.button_check);
+        mDownloadImageButton = (Button) findViewById(R.id.button_download_image);
+        mDownloadWebButton = (Button) findViewById(R.id.button_download_web);
+        mNetworkText = (TextView) findViewById(R.id.text_network);
+        mResultText = (TextView) findViewById(R.id.text_result);
+        mimageView = (ImageView) findViewById(R.id.image);
+        mCheckButton.setOnClickListener(this);
+        mDownloadImageButton.setOnClickListener(this);
+        mDownloadWebButton.setOnClickListener(this);
+
+        // 获取内部存储目录: files
+        mPrivateRootDir = getFilesDir();
+        // 获取内部存储子目录: files/images
+        mImagesDir = new File(mPrivateRootDir, "images");
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_check) {
+            checkNetworkState(); // 检查网络
+        } else if (view.getId() == R.id.button_download_image) {
+            downloadImages(); // 下载图片
+        } else if (view.getId() == R.id.button_download_web) {
+            downloadWebPage(); // 下载网页
+        }
+    }
+
+    // 检查网络状态
+    private void checkNetworkState() {
+        // 取得连接管理器
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // 检查当前激的网络
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            mConnected = true;
+        } else {
+            mConnected = false;
+        }
+
+        String types = "";
+
+        // 检查Wi-Fi
+        networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        boolean isWifiConn = networkInfo != null && networkInfo.isConnected();
+        types += isWifiConn ? "Wi-Fi" : "";
+
+        // 检查数据网络
+        networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        boolean isMobileConn = networkInfo != null && networkInfo.isConnected();
+        types += isMobileConn ? "流量" : "";
+
+        // 检查蓝牙
+        networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_BLUETOOTH);
+        boolean isBluetoothConn = networkInfo != null && networkInfo.isConnected();
+        types += isBluetoothConn ? ", 蓝牙" : "";
+
+        mNetworkText.setTextColor(mConnected ? Color.GREEN : Color.RED);
+        mNetworkText.setText(mConnected ? "网络正常 (" +types + ")" : "网络未连接!");
+    }
+
+    // 下载图片
+    private void downloadImages() {
+        // 创建下载器
+        mFileDownloader = new MainActivity6(new MainActivity6.OnImageDownloadListener() {
+            @Override
+            public void onError(String error) {
+                Toast.makeText(MainActivity5.this, error, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onProgressChange(int percent) {
+                Log.i(TAG, "当前进度 = " + percent);
+            }
+
+            @Override
+            public void onComplete(Bitmap bitmap, String imageUrl) {
+                // 下载的图片格式为PNG
+                final Bitmap.CompressFormat format = Bitmap.CompressFormat.PNG;
+                // 解析出原始文件名
+                String filename = imageUrl.replace(IMAGE_URL_PREFIX, "");
+                final File imageFile = new File(mImagesDir, filename);
+                // 将文件保存到磁盘中
+                MainActivity6.writeToDisk(imageFile, bitmap, new MainActivity6.OnBitmapSaveListener() {
+                    @Override
+                    public void onBitmapSaved() {
+                        Toast.makeText(MainActivity5.this, "文件已保存: " + imageFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
+                    }
+                    @Override
+                    public void onBitmapSaveError(String error) {
+                        Toast.makeText(MainActivity5.this, error, Toast.LENGTH_LONG).show();
+                    }
+                }, format, false);
+            }
+        });
+
+        // 下载所有文件
+        for(String imageName : imageNames) {
+            String imageUrl = IMAGE_URL_PREFIX + imageName;
+            mFileDownloader.download(imageUrl, true);
+        }
+    }
+
+    // 下载网页
+    private void downloadWebPage() {
+    }
+
+}
